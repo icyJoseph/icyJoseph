0ba6c5123bb012c8bd42ea7f787ba3f677fb21bd
icyJoseph <sephxd1234@gmail.com>
2018-04-26 17:46:19 +0200

    chore: set up testing reports

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,26 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+// browser mocks
+const localStorageMock = (function() {
+  let store = {};
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem: function(key) {
+      delete store[key];
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+})();
+
+Object.defineProperty(window, "localStorage", {
+  value: localStorageMock
+});

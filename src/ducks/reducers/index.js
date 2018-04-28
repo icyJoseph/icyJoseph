173bb0e800a07c47ae72745de97481d69e47c1e4
icyJoseph <sephxd1234@gmail.com>
2018-04-28 08:34:07 +0200

    test: trigger rootReducer to verify response

--- src/ducks/reducers/index.js
+++ src/ducks/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 
-export default combineReducers({
+const rootReducer = combineReducers({
   counter
 });
+
+export default rootReducer;
--- src/ducks/reducers/tests/index.test.js
+++ src/ducks/reducers/tests/index.test.js
@@ -1,4 +1,5 @@
 import counter, { INC } from "../counter";
+import rootReducer from "../";
 
 describe("counter", () => {
   const action = {
@@ -9,5 +10,21 @@ describe("counter", () => {
 
   it("increments the default state", () => {
     expect(counter(undefined, action)).toEqual(result);
+  });
+});
+
+describe("rootReducer", () => {
+  const expectedState = {
+    counter: 1
+  };
+
+  const action = {
+    type: INC
+  };
+
+  const state = rootReducer(undefined, action);
+
+  it("contains all the reducers", () => {
+    expect(state).toEqual(expectedState);
   });
 });

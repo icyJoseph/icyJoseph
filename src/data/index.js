af667842d09781737b926b178c0f42b48060e238
icyJoseph <sephxd1234@gmail.com>
2018-05-03 23:59:05 +0200

    feat: first set of data for landing page

---
+++ src/data/index.js
@@ -0,0 +1,73 @@
+import ReactLogo from "../logos/ReactLogo";
+import PythonLogo from "../logos/PythonLogo";
+import JavaScriptLogo from "../logos/JavaScriptLogo";
+import Me from "../logos/Me";
+
+export default {
+  firstRow: [
+    {
+      id: 0,
+      Component: ReactLogo,
+      fill: "#61DAFB",
+      backgroundFill: "#000"
+    },
+    {
+      id: 1,
+      Component: PythonLogo,
+      fill: "#000",
+      backgroundFill: "#000"
+    },
+    {
+      id: 2,
+      Component: JavaScriptLogo,
+      fill: "#F7DF1E",
+      backgroundFill: "#F7DF1E"
+    }
+  ],
+  secondRow: [
+    {
+      id: 3,
+      Component: ReactLogo,
+      fill: "#61DAFB",
+      backgroundFill: "#000"
+    },
+    {
+      id: 4,
+      Component: PythonLogo,
+      fill: "#000",
+      backgroundFill: "#000"
+    },
+    {
+      id: 5,
+      Component: Me,
+      fill: "white",
+      backgroundFill: "tomato"
+    },
+    {
+      id: 6,
+      Component: ReactLogo,
+      fill: "#61DAFB",
+      backgroundFill: "#000"
+    }
+  ],
+  thirdRow: [
+    {
+      id: 7,
+      Component: ReactLogo,
+      fill: "#61DAFB",
+      backgroundFill: "#000"
+    },
+    {
+      id: 8,
+      Component: PythonLogo,
+      fill: "#000",
+      backgroundFill: "#000"
+    },
+    {
+      id: 9,
+      Component: JavaScriptLogo,
+      fill: "#F7DF1E",
+      backgroundFill: "#F7DF1E"
+    }
+  ]
+};

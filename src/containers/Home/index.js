eca6cef4e40d6c2fee4c7c1e0a955669c43921b6
icyJoseph <sephxd1234@gmail.com>
2018-05-07 07:56:35 +0200

    refactor: abstract away JSX and make stateless

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,37 +1,52 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Media from "react-media";
-import MainTitle from "../../components/MainTitle";
-import HexGrid from "../../components/HexGrid";
+
+import { connect } from "react-redux";
+
 import Mobile from "../../components/Mobile";
+import HomeDesktopGrid from "../../components/HomeDesktopGrid";
 
-export class Home extends Component {
-  renderGrid = this.renderGrid.bind(this);
-  renderMobile = this.renderMobile.bind(this);
+import data from "../../data";
 
-  renderGrid() {
-    return <HexGrid />;
-  }
+import {
+  openSideContent,
+  closeSideContent,
+  changeContent
+} from "../../ducks/actions/sideContainer";
 
-  renderMobile() {
-    return <Mobile />;
-  }
+export const Home = props => {
+  // Consider adding another breakpoint at 1024px
+  return (
+    <Fragment>
+      <Media query={{ maxWidth: 920 }} render={() => <Mobile />} />
+      <Media
+        query={{ minWidth: 921 }}
+        render={() => <HomeDesktopGrid {...props} data={data} />}
+      />
+    </Fragment>
+  );
+};
 
-  render() {
-    return (
-      <Fragment>
-        <MainTitle title="Meet Joseph" />
-        <Media query={{ maxWidth: 920 }} render={this.renderMobile} />
-        <Media query={{ minWidth: 921 }} render={this.renderGrid} />
-      </Fragment>
-    );
-  }
-}
+export const mapStateToProps = state => {
+  return {
+    visibility: state.sideContainer.open,
+    Content: state.sideContainer.Content
+  };
+};
 
-export default Home;
+export const mapDispatchToProps = {
+  openSideContent,
+  closeSideContent,
+  changeContent
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 Home.propTypes = {
-  location: PropTypes.object,
-  match: PropTypes.object,
-  history: PropTypes.object
+  openSideContent: PropTypes.func,
+  closeSideContent: PropTypes.func,
+  changeContent: PropTypes.func,
+  visibility: PropTypes.bool,
+  Content: PropTypes.func
 };

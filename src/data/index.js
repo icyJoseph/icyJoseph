78e718169fc8bfc85da98439d661aa439215afe7
icyJoseph <sephxd1234@gmail.com>
2018-05-07 07:55:52 +0200

    feat: add Content Key

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactLogo from "../logos/ReactLogo";
 import PythonLogo from "../logos/PythonLogo";
 import JavaScriptLogo from "../logos/JavaScriptLogo";
@@ -9,19 +10,22 @@ export default {
       id: 0,
       Component: ReactLogo,
       fill: "#61DAFB",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>React Info</p>
     },
     {
       id: 1,
       Component: PythonLogo,
       fill: "#000",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>Python Info</p>
     },
     {
       id: 2,
       Component: JavaScriptLogo,
       fill: "#F7DF1E",
-      backgroundFill: "#F7DF1E"
+      backgroundFill: "#F7DF1E",
+      Content: <p>Javascript Info</p>
     }
   ],
   secondRow: [
@@ -29,25 +33,29 @@ export default {
       id: 3,
       Component: ReactLogo,
       fill: "#61DAFB",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>React Info</p>
     },
     {
       id: 4,
       Component: PythonLogo,
       fill: "#000",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>Python Info</p>
     },
     {
       id: 5,
       Component: Me,
       fill: "white",
-      backgroundFill: "tomato"
+      backgroundFill: "tomato",
+      Content: <p>Info About me</p>
     },
     {
       id: 6,
       Component: ReactLogo,
       fill: "#61DAFB",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>React Info</p>
     }
   ],
   thirdRow: [
@@ -55,19 +63,22 @@ export default {
       id: 7,
       Component: ReactLogo,
       fill: "#61DAFB",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>React Info</p>
     },
     {
       id: 8,
       Component: PythonLogo,
       fill: "#000",
-      backgroundFill: "#000"
+      backgroundFill: "#000",
+      Content: <p>Python Info</p>
     },
     {
       id: 9,
       Component: JavaScriptLogo,
       fill: "#F7DF1E",
-      backgroundFill: "#F7DF1E"
+      backgroundFill: "#F7DF1E",
+      Content: <p>Javascript Info</p>
     }
   ]
 };

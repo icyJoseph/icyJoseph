9b5cf6973861d6ac8c1ba4e38f12ad115bf12c6b
icyJoseph <sephxd1234@gmail.com>
2018-05-11 18:52:35 +0200

    refactor: turn Mobile into a container

--- src/containers/Mobile/index.js
+++ src/containers/Mobile/index.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
-import MainTitle from "../MainTitle";
+import MainTitle from "../../components/MainTitle";
 
 export const Mobile = () => {
   return (
     <Fragment>
       <MainTitle title="Meet Joseph" />
-      <p>Mobile View</p>;
+      <p>Mobile View</p>
     </Fragment>
   );
 };

4c9e8eb04b4e24a2b3305e5fcba923e3b6007549
icyJoseph <sephxd1234@gmail.com>
2018-05-12 07:37:18 +0200

    feat: implement async components

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,34 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Loadable from "react-loadable";
 import TopMenu from "./containers/TopMenu";
-import App from "./containers/App";
-import Home from "./containers/Home";
-import NoMatch from "./containers/NoMatch";
+
+import HomeLoading from "./components/Loading";
+
+const asyncHome = Loadable({
+  loader: () => import("./containers/Home"),
+  loading: HomeLoading
+});
+
+const asyncApp = Loadable({
+  loader: () => import("./containers/App"),
+  loading: HomeLoading
+});
+
+const asyncNoMatch = Loadable({
+  loader: () => import("./containers/NoMatch"),
+  loading: HomeLoading
+});
 
 export const Routes = () => (
   <BrowserRouter>
     <Fragment>
       <TopMenu />
       <Switch>
-        <Route path="/blog" component={App} />
-        <Route path="/hacks" component={App} />
-        <Route exact path="/" component={Home} />
-        <Route component={NoMatch} />
+        <Route path="/blog" component={asyncApp} />
+        <Route path="/hacks" component={asyncApp} />
+        <Route path="/" component={asyncHome} />
+        <Route component={asyncNoMatch} />
       </Switch>
     </Fragment>
   </BrowserRouter>

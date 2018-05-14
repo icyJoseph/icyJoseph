7ead7b74b4a6f1575c586ca6d847f0dd4058e992
icyJoseph <sephxd1234@gmail.com>
2018-05-14 07:33:11 +0200

    feat: add body/header loader

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,37 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Loadable from "react-loadable";
-import TopMenu from "./containers/TopMenu";
-
-import HomeLoading from "./components/Loading";
+import BodyLoader from "./components/Loading/BodyLoader";
+import HeaderLoader from "./components/Loading/HeaderLoader";
 
 const asyncHome = Loadable({
   loader: () => import("./containers/Home"),
-  loading: HomeLoading
+  loading: BodyLoader,
+  delay: 600
 });
 
 const asyncApp = Loadable({
   loader: () => import("./containers/App"),
-  loading: HomeLoading
+  loading: BodyLoader,
+  delay: 600
 });
 
 const asyncNoMatch = Loadable({
   loader: () => import("./containers/NoMatch"),
-  loading: HomeLoading
+  loading: BodyLoader,
+  delay: 600
+});
+
+const AsyncTopMenu = Loadable({
+  loader: () => import("./containers/TopMenu"),
+  loading: HeaderLoader,
+  delay: 600
 });
 
 export const Routes = () => (
   <BrowserRouter>
     <Fragment>
-      <TopMenu />
+      <AsyncTopMenu />
       <Switch>
         <Route path="/blog" component={asyncApp} />
         <Route path="/hacks" component={asyncApp} />

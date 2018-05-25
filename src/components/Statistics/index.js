6b260fcff4bf9162bb6600bed6e7a5cd3c83e52e
icyJoseph <sephxd1234@gmail.com>
2018-05-25 06:20:50 +0200

    feat: add countup

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
-import { Icon, Statistic } from "semantic-ui-react";
+import CountUp from "react-countup";
+import Statistic from "semantic-ui-react/dist/commonjs/views/Statistic";
+import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 
 export const Statistics = () => (
   <Fragment>
@@ -7,26 +9,34 @@ export const Statistics = () => (
       <Statistic>
         <Statistic.Value>
           <Icon name="github" />
-          5
+          <CountUp start={0} end={5} />
         </Statistic.Value>
         <Statistic.Label>Repos</Statistic.Label>
       </Statistic>
       <Statistic>
-        <Statistic.Value>42</Statistic.Value>
-        <Statistic.Label>Repos</Statistic.Label>
+        <Statistic.Value>
+          <CountUp start={0} end={4} />
+        </Statistic.Value>
+        <Statistic.Label>Hacks</Statistic.Label>
       </Statistic>
       <Statistic>
-        <Statistic.Value>700</Statistic.Value>
+        <Statistic.Value>
+          <CountUp start={0} end={700} />
+        </Statistic.Value>
         <Statistic.Label>Commits</Statistic.Label>
       </Statistic>
     </Statistic.Group>
     <Statistic.Group inverted widths="two" style={{ margin: "15px 0" }}>
       <Statistic>
-        <Statistic.Value>80000</Statistic.Value>
+        <Statistic.Value>
+          <CountUp start={0} end={8000} />
+        </Statistic.Value>
         <Statistic.Label>Javascript Bytes</Statistic.Label>
       </Statistic>
       <Statistic>
-        <Statistic.Value>1000</Statistic.Value>
+        <Statistic.Value>
+          <CountUp start={0} end={1000} />
+        </Statistic.Value>
         <Statistic.Label>Python Bytes</Statistic.Label>
       </Statistic>
     </Statistic.Group>

31f236aca0c08ec519cad12ea8890a53ef0d35fc
icyJoseph <sephxd1234@gmail.com>
2018-05-26 06:44:46 +0200

    chore: switch to use semantic ui from CDN

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import Routes from "./routes";
 import registerServiceWorker from "./registerServiceWorker";

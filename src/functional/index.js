c644b3f2ec36c96144b3cdba7c26f95dc969de76
icyJoseph <sephxd1234@gmail.com>
2018-06-10 06:59:07 +0200

    feat: add functional candy box

--- src/functional/index.js
+++ src/functional/index.js
@@ -1,2 +1,25 @@
+// Pipe
+export const pipe = (...funcs) => (...args) =>
+  funcs.reduce((val, func, i) => (i === 0 ? func(...val) : func(val)), args);
+
+// curry
+export const curry = f => a => (...b) => f(a, ...b);
+
 // Take the first n elements of an array
 export const take = n => array => array.slice(0, n);
+
+// Apply the same argument against many functions
+export const mapValueToFunctions = (...funcs) => arg =>
+  funcs.map(func => func(arg));
+
+// Flatten
+export const flatten = arr => [].concat(...arr);
+
+// Sort descending
+export const sort = (arr, type) => arr.sort((a, b) => b[type] - a[type]);
+
+// Map
+export const mapf = (func, arr) => arr.map(func);
+
+// Filter
+export const filterf = (func, arr) => arr.filter(func);

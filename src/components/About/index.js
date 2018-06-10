a9fcfe62115a6ecae9ea0b4b8b05f6300442f507
icyJoseph <sephxd1234@gmail.com>
2018-06-10 07:50:21 +0200

    test: update snapshots

---
+++ src/components/About/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const About = ({ value }) => {
+  return <div>Info about me</div>;
+};
+
+export default About;
---
+++ src/components/Certified/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Certified = () => {
+  return <div>React Certification</div>;
+};

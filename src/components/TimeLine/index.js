ce95a25882b78c30c3da9d6806d5d9124c428506
icyJoseph <sephxd1234@gmail.com>
2018-06-10 07:05:40 +0200

    refactor: Use meta as key and pass last props

--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -4,12 +4,15 @@ import Event from "./Event";
 
 export const TimeLine = ({ data }) => {
   return (
-    <div>
-      <div style={{ marginBottom: "50px" }}>
-        {data.map((event, index) => (
-          <Event key={event.id} left={index % 2 === 0} {...event} />
-        ))}
-      </div>
+    <div style={{ marginBottom: "50px" }}>
+      {data.map(({ meta, ...rest }, index) => (
+        <Event
+          key={meta}
+          last={index + 1 === data.length}
+          meta={meta}
+          {...rest}
+        />
+      ))}
     </div>
   );
 };

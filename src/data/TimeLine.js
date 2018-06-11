51cc057191d0fde17882740a5ec97646b31cedc3
icyJoseph <sephxd1234@gmail.com>
2018-06-11 06:32:48 +0200

    feat: basic timeline data

---
+++ src/data/TimeLine.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    title: "April, 2018",
+    meta: "CodeJam",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog
+      The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "March, 2018",
+    meta: "Hash Code",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "January, 2018",
+    meta: "React Certificate",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "January, 2018",
+    meta: "Volvo Group",
+    fill: "#C92F00",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "November, 2017",
+    meta: "Open Hack Winner",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "October, 2016",
+    meta: "Consultant",
+    fill: "#292929",
+    backgroundFill: "#5159BF",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "June, 2016",
+    meta: "M.Sc. in Science with Major in Entepreneurship and Business Design",
+    fill: "#292929",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "January, 2016",
+    meta: "CTO, Co-founder",
+    fill: "#F0DB4F",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "March, 2013",
+    meta: "Project Engineer",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  },
+  {
+    title: "December, 2012",
+    meta: "B.Sc. in Science with Major in Electronics Engineering",
+    fill: "#273B7A",
+    backgroundFill: "tomato",
+    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+  }
+];

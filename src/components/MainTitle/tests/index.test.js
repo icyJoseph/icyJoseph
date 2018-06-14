7973a4a3e9c2fb7400ff22723fa43ffa6910a90e
icyJoseph <sephxd1234@gmail.com>
2018-06-14 08:47:36 +0200

    test: behaviour unit test for MainTitle

--- src/components/MainTitle/tests/index.test.js
+++ src/components/MainTitle/tests/index.test.js
@@ -5,7 +5,6 @@ import MainTitle from "../";
 describe("MainTitle", () => {
   const props = { title: "Test Title" };
   const spy = jest.spyOn(MainTitle.prototype, "componentDidMount");
-  const revealSpy = jest.spyOn(MainTitle.prototype, "revealTitle");
   const wrapper = shallow(<MainTitle {...props} />);
   it("renders", () => {
     expect(wrapper).toMatchSnapshot();
@@ -13,7 +12,23 @@ describe("MainTitle", () => {
   });
 
   it("changes state when revealSpy is called", () => {
-    const initialState = wrapper.state(["reveal"]);
+    const initialState = wrapper.state("reveal");
+    wrapper.instance().revealTitle();
+    expect(wrapper.state(["reveal"])).toEqual(!initialState);
+  });
+});
+
+describe("Desktop MainTitle", () => {
+  const props = { title: "Test Title", desktop: true };
+  const spy = jest.spyOn(MainTitle.prototype, "componentDidMount");
+  const wrapper = shallow(<MainTitle {...props} />);
+  it("renders", () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("changes state when revealSpy is called", () => {
+    const initialState = wrapper.state("reveal");
     wrapper.instance().revealTitle();
     expect(wrapper.state(["reveal"])).toEqual(!initialState);
   });

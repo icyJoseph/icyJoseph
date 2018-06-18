d5a3ec6f5b9f99b756c891e81ae90dcac4b7b261
icyJoseph <sephxd1234@gmail.com>
2018-06-18 07:36:07 +0200

    feat: implement About card

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Card, Icon } from "semantic-ui-react";
+import photo from "./profile.png";
+import { fetchUserData } from "../../ducks/github";
+import { shouldFetch } from "../../helpers";
 
-export const About = ({ value }) => {
-  return <div>Info about me</div>;
+const Extra = ({ repos, gists, company }) => (
+  <div style={{ display: "flex", flexDirection: "row" }}>
+    <a
+      key="repos"
+      style={{ display: "flex", flex: 1 }}
+      href="https://github.com/icyJoseph"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon name="github" color="black" />
+      {repos} repos
+    </a>
+    <a
+      key="gists"
+      style={{ display: "flex", flex: 1 }}
+      href="https://gist.github.com/icyJoseph"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon name="code" color="black" />
+      {gists} gists
+    </a>
+    <a
+      key="company"
+      style={{ display: "flex", flex: 1 }}
+      href="http://skill-it.se/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon name="briefcase" color="black" />
+      {company}
+    </a>
+  </div>
+);
+
+export class About extends Component {
+  componentDidMount() {
+    const { expiry } = this.props;
+    return shouldFetch(expiry) && this.props.fetchUserData("icyJoseph");
+  }
+
+  render() {
+    const { user } = this.props;
+    return (
+      <Card
+        image={photo}
+        header={`${user.name}`}
+        meta="Front-end developer"
+        description="Peruvian living in Gothenburg, Sweden. React Ninja, JavaScript enthusiast, enjoys football and swimming."
+        extra={
+          <Extra
+            repos={user.public_repos}
+            gists={user.public_gists}
+            company={user.company}
+          />
+        }
+      />
+    );
+  }
+}
+
+const mapStateToProps = ({ github: { user }, expiry }) => {
+  return {
+    expiry,
+    user
+  };
 };
 
-export default About;
+const mapDispatchToProps = {
+  fetchUserData
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);

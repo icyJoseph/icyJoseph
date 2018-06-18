67291f2b0540774ceb1a6f9c029f4ac3f33b44c4
icyJoseph <sephxd1234@gmail.com>
2018-06-18 07:38:13 +0200

    feat: add mobile events description

--- src/data/TimeLine.js
+++ src/data/TimeLine.js
@@ -4,70 +4,77 @@ export default [
     meta: "CodeJam",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog
-      The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I took part of CodeJam 2018. I managed to pass the qualification round, scoring 55/100 points.`
   },
   {
     title: "March, 2018",
     meta: "Hash Code",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I was part of a team in HashCode 2018, during the first 3 hours we scored 12 million points. During the 
+      extended round we raised the score to 47 million points.`
   },
   {
     title: "January, 2018",
     meta: "React Certificate",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I finished a React nano-degree from Udacity, one and a half month earlier than needed. The nano-degree 
+      included React, React-Redux and React-Native.`
   },
   {
     title: "January, 2018",
     meta: "Volvo Group",
     fill: "#C92F00",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I began working at Volvo Group Connected Solutions, as a React developer for their fleet management system`
   },
   {
     title: "November, 2017",
     meta: "Open Hack Winner",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I particated in Open Hack Gothenburg, 2017. A hackaton over the weekend. We build CrowdPath, a tool to
+      help immigrants share and rate information the immigration process.`
   },
   {
     title: "October, 2016",
     meta: "Consultant",
     fill: "#292929",
     backgroundFill: "#5159BF",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `I worked as a consultant for Sigma Group. Our client in this assignment was Ericsson, Gothenburg.`
   },
   {
     title: "June, 2016",
     meta: "M.Sc. in Science with Major in Entepreneurship and Business Design",
     fill: "#292929",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `Obtained a Masters Degree from Chalmers University. As part of the education we studied, intellectual
+      property, business management, business design, and even started a company during the second year.`
   },
   {
     title: "January, 2016",
     meta: "CTO, Co-founder",
     fill: "#F0DB4F",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `Started Enatics Sports AB, a by product of the Chalmers University program I coursed. The company wanted
+      to create a connected device for racket sports, and also provide image analysis tools to augment the game experience.`
   },
   {
     title: "March, 2013",
     meta: "Project Engineer",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `Worked as a project engineer both on site and office. I took the design and implementation of the
+      control system used to start the loading process across 3 kilometers of a mineral deliverying conveyor, at 
+      Callao Port in Lima, Peru.`
   },
   {
     title: "December, 2012",
     meta: "B.Sc. in Science with Major in Electronics Engineering",
     fill: "#273B7A",
     backgroundFill: "tomato",
-    description: `The description goes here. The quick brown fox, jumps over the lazy dog`
+    description: `Graduated from the electronics engineering program. I mainly focused in telecommunications 
+      and control topics.`
   }
 ];

6b936352d8a7bdfff6dc4a53edfbc86dfbb71244
icyJoseph <sephxd1234@gmail.com>
2018-06-18 08:13:03 +0200

    feat: add repos topics actions

--- src/ducks/github/index.js
+++ src/ducks/github/index.js
@@ -7,6 +7,8 @@ export const FAILED_REPOS = "failed_repos";
 export const FAILED_USER_DATA = "failed_repos";
 export const LOAD_TOTAL_CONTRIBUTIONS = "load_total_contributions";
 export const LOAD_LANGUAGES = "load_languages";
+export const FETCH_REPOS_TOPICS = "fetch_repos_topics";
+export const SUCCESS_REPOS_TOPICS = "success_repos_topics";
 
 // Actions
 export const fetchUserData = user => {
@@ -37,7 +39,8 @@ export default function reducer(
     languages: [],
     loadingRepos: false,
     loadingUser: false,
-    expiry: ""
+    expiry: "",
+    topics: []
   },
   { type, payload, ...rest }
 ) {
@@ -59,6 +62,8 @@ export default function reducer(
       return { ...github, commits: payload };
     case LOAD_LANGUAGES:
       return { ...github, languages: payload };
+    case SUCCESS_REPOS_TOPICS:
+      return { ...github, topics: payload };
     default:
       return { ...github };
   }

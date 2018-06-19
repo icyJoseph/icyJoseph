1b7005339395987226752b137f53a84f88bcb5f2
icyJoseph <sephxd1234@gmail.com>
2018-06-19 08:30:25 +0200

    feat: use token to get more requests from GitHub

--- src/ducks/gist/api.js
+++ src/ducks/gist/api.js
@@ -4,12 +4,19 @@ import { curry, curryRight, get, head, keys, pipe } from "../../functional";
 import { github } from "../github/api";
 
 export const getGist = gist => {
-  return axios.get(`${github}/gists/${gist}`).then(({ data: { files } }) => {
-    return pipe(
-      keys,
-      head,
-      curry(get)(files),
-      curryRight(get)("content")
-    )(files);
-  });
+  return axios
+    .get(`${github}/gists/${gist}`, {
+      auth: {
+        username: "icyJoseph",
+        password: `${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    })
+    .then(({ data: { files } }) => {
+      return pipe(
+        keys,
+        head,
+        curry(get)(files),
+        curryRight(get)("content")
+      )(files);
+    });
 };
--- src/ducks/github/api.js
+++ src/ducks/github/api.js
@@ -1,26 +1,32 @@
 import axios from "axios";
 
 export const github = `https://api.github.com`;
+const auth = {
+  auth: {
+    username: "icyJoseph",
+    password: `${process.env.REACT_APP_GITHUB_TOKEN}`
+  }
+};
 
 export const getUserRepos = user => {
   return axios
-    .get(`${github}/users/${user}/repos`)
+    .get(`${github}/users/${user}/repos`, auth)
     .then(({ data }) => [...data]);
 };
 
 export const getUser = user => {
-  return axios.get(`${github}/users/${user}`).then(({ data }) => data);
+  return axios.get(`${github}/users/${user}`, auth).then(({ data }) => data);
 };
 
 export const getRepoContributors = (user, repo) => {
   return axios
-    .get(`${github}/repos/${user}/${repo}/contributors`)
+    .get(`${github}/repos/${user}/${repo}/contributors`, auth)
     .then(({ data }) => [...data]);
 };
 
 export const getRepoLanguages = (user, repo) => {
   return axios
-    .get(`${github}/repos/${user}/${repo}/languages`)
+    .get(`${github}/repos/${user}/${repo}/languages`, auth)
     .then(({ data }) => data);
 };
 
@@ -29,6 +35,10 @@ export const getRepoTopics = (user, repo) => {
     .get(`${github}/repos/${user}/${repo}/topics`, {
       headers: {
         Accept: "application/vnd.github.mercy-preview+json"
+      },
+      auth: {
+        username: "icyJoseph",
+        password: `${process.env.REACT_APP_GITHUB_TOKEN}`
       }
     })
     .then(({ data: { names } }) => ({

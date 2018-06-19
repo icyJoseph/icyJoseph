de268136d65fad5bed457117c22de9f3d6f4f7c1
icyJoseph <sephxd1234@gmail.com>
2018-06-19 17:19:40 +0200

    feat: bring title from data

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Card, Icon } from "semantic-ui-react";
+import { capitalize } from "../../functional";
 
 const description = [
   "Amy is a violinist with 2 years experience in the wedding industry.",
   "She enjoys the outdoors and currently resides in upstate New York."
 ].join(" ");
 
-export const Common = ({ meta }) => {
-  console.log(meta);
+export const Common = ({ title }) => {
   return (
     <Card>
-      <Card.Content header="About Amy" />
+      <Card.Content header={capitalize(title)} />
       <Card.Content description={description} />
       <Card.Content extra>
         <Icon name="user" />

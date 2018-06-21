0fd3fd9cc74d7d6abab5bb1d9edc3beea6d925b5
icyJoseph <sephxd1234@gmail.com>
2018-06-21 08:44:32 +0200

    feat: add more topics to filterCriteria

--- src/components/Portfolio/helpers.js
+++ src/components/Portfolio/helpers.js
@@ -4,6 +4,12 @@ export const filterCriteria = (topics, meta) => {
       return !topics.includes("redux") && topics.includes("react");
     case "redux":
       return topics.includes("redux");
+    case "hackathon":
+      return topics.includes("hackathon");
+    case "python":
+      return topics.includes("python");
+    case "algorithms":
+      return topics.includes("algorithms");
     default: {
       return !topics.includes("redux") && topics.includes("react");
     }

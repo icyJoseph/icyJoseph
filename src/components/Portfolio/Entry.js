509bb70c325519abc7b293a48ade855489810e8c
icyJoseph <sephxd1234@gmail.com>
2018-06-21 08:43:56 +0200

    refactor: Item.Group should leave outside

--- src/components/Portfolio/Entry.js
+++ src/components/Portfolio/Entry.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { Button, Icon, Item, Label } from "semantic-ui-react";
 
-export const Entry = ({ name, description, topics, html_url, language }) => {
+export const Entry = ({ name, description, topics, html_url }) => {
   return (
-    <Item.Group divided>
-      <Item>
-        <Item.Content>
-          <Item.Header as="a">{name}</Item.Header>
-          <Item.Description>
-            <p>{description}</p>
-          </Item.Description>
-          <Item.Extra>
-            {topics.map(topic => <Label key={topic}>{topic}</Label>)}
-            <Button
-              primary
-              floated="right"
-              href={html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              To Repo
-              <Icon name="right chevron" />
-            </Button>
-          </Item.Extra>
-        </Item.Content>
-      </Item>
-    </Item.Group>
+    <Item>
+      <Item.Content>
+        <Item.Header style={{ color: "white" }}>{name}</Item.Header>
+        <Item.Description style={{ color: "white" }}>
+          <p>{description}</p>
+        </Item.Description>
+        <Item.Extra>
+          {topics.map(topic => <Label key={topic}>{topic}</Label>)}
+          <Button
+            primary
+            floated="right"
+            href={html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            To Repo
+            <Icon name="right chevron" />
+          </Button>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
   );
 };
 

fe8ff655c9a7579e38751c86739f88f7bda86402
icyJoseph <sephxd1234@gmail.com>
2018-06-21 08:46:41 +0200

    feat: add more colors and name constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
+// colors
 export const orange = "rgb(255, 157,0)";
 export const golden = "rgb(258,198,0)";
 export const white = "rgb(255, 255, 255)";
+export const transparent = "rgba(0,0,0,0)";
+export const darkBlue = "#193549";
+
+// names
+
+export const icyJoseph = "icyJoseph";

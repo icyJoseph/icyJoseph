9b61633fa922d64bfe5bbe7ee49165a7cfce552b
icyJoseph <sephxd1234@gmail.com>
2018-07-10 08:32:41 +0200

    feat: async componentDidMount to search repos/homepage, or declare 404

--- src/containers/NoMatch/index.js
+++ src/containers/NoMatch/index.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const NoMatch = () => {
-  return <div>404</div>;
-};
+import { getUserRepos } from "../../ducks/github/api";
+import { getToken } from "../../ducks/auth/api";
+import { curryRight, head, take, pipe, split } from "../../functional";
 
+import Searching from "./Searching";
+import NotFound from "./NotFound";
+
+const takeOne = take(1, 1);
+const splitDash = curryRight(split)("/");
+
+export class NoMatch extends Component {
+  state = {
+    url: null
+  };
+
+  setAsyncState(state) {
+    return new Promise(resolve => this.setState(state, resolve));
+  }
+
+  async componentDidMount() {
+    const {
+      location: { pathname }
+    } = this.props;
+
+    const possibleRepo = pipe(
+      splitDash,
+      takeOne,
+      head
+    )(pathname);
+
+    const token = await getToken();
+    const repos = await getUserRepos("icyJoseph", token);
+
+    const existingRepo = repos.find(
+      ({ name, homepage }) => name === possibleRepo && homepage
+    );
+
+    return this.setAsyncState({
+      url: existingRepo && existingRepo.homepage
+    });
+  }
+  render() {
+    const { url } = this.state;
+
+    if (url === undefined) return <NotFound />;
+    return url !== undefined && <Searching url={url} />;
+  }
+}
 export default NoMatch;

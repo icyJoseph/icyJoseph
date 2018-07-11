aa8f957942f94efc116036fcd39674d9071d9d2e
icyJoseph <sephxd1234@gmail.com>
2018-07-11 17:22:17 +0200

    feat: implement desktop mode

--- src/containers/Tablet/styled/index.js
+++ src/containers/Tablet/styled/index.js
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import { shadow } from "../../../constants";
 
 export const TabletWrapper = styled.div`
-  height: 100%;
+  height: ${props => (props.desktop ? "100vh" : "100%")};
   background: ${props => (props.desktop ? shadow : "dodgerblue")};
-  z-index: 1px;
+  z-index: 1;
 `;
 
 export const Background = styled.div`
   position: absolute;
   background-image: ${props => `url(${props.background})`};
-  background-size: cover;
+  background-size: 100% 100%;
   width: 100%;
   height: 100%;
   z-index: -1;

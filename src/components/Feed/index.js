93b7f6e9c0be13046254881ecb5e06d55a7c1ac2
icyJoseph <sephxd1234@gmail.com>
2018-07-11 18:20:36 +0200

    feat: show image preview

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Button, Icon, Card, Label } from "semantic-ui-react";
+import {
+  Button,
+  Icon,
+  Card,
+  Feed as Content,
+  Label,
+  Image
+} from "semantic-ui-react";
+import Media from "react-media";
 import Bio from "./Bio";
 
 export const imageSrc = "https://miro.medium.com/fit/c/240/240";
@@ -9,32 +17,45 @@ export const Feed = ({ title, uniqueSlug, virtuals, content }) => (
   <Card fluid>
     <Card.Content>
       <Card.Header>{title}</Card.Header>
-      <Card.Description>{content.subtitle}</Card.Description>
-      <Card.Meta>
-        <Label.Group>
-          <Label>
-            <Icon name="book" /> {virtuals.wordCount} words
-          </Label>
-        </Label.Group>
-        <Label.Group>
-          {virtuals.tags.map(tag => (
-            <Label key={tag.slug} color={"blue"}>
-              {tag.name}
+      <Content>
+        <Media
+          query="(min-width: 689px)"
+          render={() => (
+            <Image
+              circular
+              floated="left"
+              style={{ width: "150px" }}
+              src={`${imageSrc}/${virtuals.previewImage.imageId}`}
+            />
+          )}
+        />
+        <Card.Description>{content.subtitle}</Card.Description>
+        <Card.Meta>
+          <Label.Group>
+            <Label>
+              <Icon name="book" /> {virtuals.wordCount} words
             </Label>
-          ))}
-        </Label.Group>
-      </Card.Meta>
-      <Button
-        basic
-        color="blue"
-        floated="right"
-        href={`${medium}/${uniqueSlug}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        To article
-        <Icon name="right chevron" />
-      </Button>
+          </Label.Group>
+          <Label.Group>
+            {virtuals.tags.map(tag => (
+              <Label key={tag.slug} color={"blue"}>
+                {tag.name}
+              </Label>
+            ))}
+          </Label.Group>
+        </Card.Meta>
+        <Button
+          basic
+          color="blue"
+          floated="right"
+          href={`${medium}/${uniqueSlug}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          To article
+          <Icon name="right chevron" />
+        </Button>
+      </Content>
     </Card.Content>
   </Card>
 );

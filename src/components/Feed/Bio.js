a9c83d0864678c03d6d963af0498f3fb6f5edf80
icyJoseph <sephxd1234@gmail.com>
2018-07-11 18:01:18 +0200

    feat: implement responsive bio

--- src/components/Feed/Bio.js
+++ src/components/Feed/Bio.js
@@ -1,28 +1,37 @@
 import React from "react";
+import Media from "react-media";
 import { Header, Icon, Image, Button } from "semantic-ui-react";
 
 const mediumFeed = "https://medium.com/@icjoseph/?feed=latest";
-const imageSrc = "https://miro.medium.com/fit/c/192/192";
+const imageSrc = "https://miro.medium.com/fit/c/240/240";
 
 export const Bio = ({ imageId, name, username, bio }) => (
-  <Header as="h1" size="huge" dividing color="blue">
-    <Image circular src={`${imageSrc}/${imageId}`} />
+  <Header as="h1" block size="huge" dividing color="blue">
+    <Media
+      query="(min-width: 579px)"
+      render={() => <Image circular src={`${imageSrc}/${imageId}`} />}
+    />
     <Header.Content>
       {name}
       <Header.Subheader>{username}</Header.Subheader>
       <Header.Subheader>{bio}</Header.Subheader>
     </Header.Content>
-    <Button
-      basic
-      color="blue"
-      floated="right"
-      href={mediumFeed}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      To Medium
-      <Icon name="right chevron" />
-    </Button>
+    <Media
+      query="(min-width: 515px)"
+      render={() => (
+        <Button
+          basic
+          color="blue"
+          floated="right"
+          href={mediumFeed}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          To Medium
+          <Icon name="right chevron" />
+        </Button>
+      )}
+    />
   </Header>
 );
 

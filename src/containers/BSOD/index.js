5505c0aece994434bcc8b85edeee6c89ec7afd48
icyJoseph <sephxd1234@gmail.com>
2018-10-02 20:38:11 +0200

    feat: Blue screen of death on IE browser

---
+++ src/containers/BSOD/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background: dodgerblue;
+  color: white;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+`;
+
+const Container = styled.div`
+  margin-top: 100px;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const SadFace = styled.div`
+  font-size: 48pt;
+  margin: 40px;
+`;
+
+const Error = styled.span`
+  background: #fff;
+  color: dodgerblue;
+  padding: 2px 8px;
+  font-weight: bold;
+`;
+
+const Menu = styled.div`
+  text-align: center;
+  margin-top: 50px;
+`;
+
+const Paragraph = styled.p`
+  margin: 30px 100px;
+  text-align: center;
+`;
+
+const Link = styled.a`
+  margin: 10px;
+  color: white;
+  text-decoration: underline;
+`;
+
+const BSOD = () => (
+  <Wrapper>
+    <Container>
+      <Error>ERROR IE DETECTED</Error>
+      <SadFace>:(</SadFace>
+      <Paragraph>
+        Unfortunately, I do not have access to a computer with IE.
+        <br />
+        You can either wait until I do, or download another browser.
+      </Paragraph>
+      <Paragraph>* Do not send me an e-mail to notify about this.</Paragraph>
+      <Paragraph>You can instead visit my GitHub page.</Paragraph>
+      <Menu>
+        <Link
+          href="https://github.com/icyJoseph"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </Link>
+        <Link
+          href="https://www.google.com/chrome/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chrome
+        </Link>
+      </Menu>
+    </Container>
+  </Wrapper>
+);
+
+export default BSOD;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,35 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import Routes from "./routes";
+import BSOD from "./containers/BSOD";
 import registerServiceWorker from "./registerServiceWorker";
 import configureStore from "./ducks/store/";
 
-if ("ontouchstart" in document.documentElement) {
-  const highlight = "-webkit-tap-highlight-color";
-  document.body.style.cursor = "pointer";
-  document.body.style[highlight] = "transparent";
+const userAgent = window.navigator.userAgent;
+const userHasIE = /MSIE|Trident/.test(userAgent);
+const appRoot = document.getElementById("root");
+
+if (userHasIE) {
+  ReactDOM.render(<BSOD />, appRoot);
+} else {
+  if ("ontouchstart" in document.documentElement) {
+    const highlight = "-webkit-tap-highlight-color";
+    document.body.style.cursor = "pointer";
+    document.body.style[highlight] = "transparent";
+  }
+
+  const storedState = localStorage.getItem("state");
+
+  const store = configureStore(
+    storedState ? JSON.parse(storedState) : undefined
+  );
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <Routes />
+    </Provider>,
+    appRoot
+  );
+
+  registerServiceWorker();
 }
-
-const storedState = localStorage.getItem("state");
-
-const store = configureStore(storedState ? JSON.parse(storedState) : undefined);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById("root")
-);
-
-registerServiceWorker();

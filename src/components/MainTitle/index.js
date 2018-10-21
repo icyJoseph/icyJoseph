904a0f57a136830c7c1803bb67bb8970eb697da2
icyJoseph <sephxd1234@gmail.com>
2018-10-21 21:49:42 +0200

    feat: do not reveal text anymore, add subheader

--- src/components/MainTitle/index.js
+++ src/components/MainTitle/index.js
@@ -1,65 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Header } from "semantic-ui-react";
-import ReactRevealText from "react-reveal-text";
 import { TitleContainer } from "./styled";
 
-class MainTitle extends Component {
-  state = { reveal: false };
-
-  revealTitle = this.revealTitle.bind(this);
-
-  componentDidMount() {
-    const { timeout = 1000 } = this.props;
-    this.timer = setTimeout(this.revealTitle, timeout);
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
-
-  revealTitle() {
-    return this.setState({ reveal: true });
-  }
-
-  render() {
-    const {
-      title,
-      desktop,
-      timeout = 500,
-      delayMin = 200,
-      delayMax = 1200
-    } = this.props;
-    return (
-      <TitleContainer>
-        <Header as="h1" size="huge" textAlign="center">
-          <ReactRevealText
-            show={this.state.reveal}
-            style={{
-              ...style,
-              letterSpacing: desktop ? "0.7em" : "0.5em",
-              color: desktop ? "black" : "FloralWhite"
-            }}
-            transitionTime={timeout}
-            delayMin={delayMin}
-            delayMax={delayMax}
-          >
-            {title}
-          </ReactRevealText>
-        </Header>
-      </TitleContainer>
-    );
-  }
-}
+const MainTitle = ({ title, subtitle, as = "h1", center }) => (
+  <TitleContainer center={center}>
+    <Header
+      as={as}
+      size="huge"
+      textAlign={center ? "center" : "left"}
+      style={style}
+    >
+      {title}
+      {subtitle && (
+        <Header.Subheader style={{ color: "#f8f8f8" }}>
+          {subtitle}
+        </Header.Subheader>
+      )}
+    </Header>
+  </TitleContainer>
+);
 
 MainTitle.propTypes = {
   title: PropTypes.string.isRequired,
-  desktop: PropTypes.bool
+  subtitle: PropTypes.string,
+  as: PropTypes.string,
+  center: PropTypes.bool
 };
 
 const style = {
   fontSize: "35px",
-  lineHeight: "36px"
+  lineHeight: "36px",
+  color: "white"
 };
 
 export default MainTitle;
--- src/components/MainTitle/styled/index.js
+++ src/components/MainTitle/styled/index.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 export const TitleContainer = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: ${({ center }) => (center ? "center" : "flex-start")};
+  align-items: ${({ center }) => (center ? "center" : "flex-start")};
   min-height: 90px;
+  padding: 0 20px;
 `;

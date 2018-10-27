430ca4e7751a4ab3c924ac99032da760e9355aa7
icyJoseph <sephxd1234@gmail.com>
2018-10-27 05:07:40 +0200

    feat: add background to Blog

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -1,19 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Container, Card, Loader } from "semantic-ui-react";
-
+import { Background, Mask } from "../../components/Background";
 import Feed, { Bio } from "../../components/Feed";
 import { fetchFeed } from "../../ducks/medium";
-import { shouldFetch } from "../../helpers/";
+import { shouldFetch, setUpMediaQuery } from "../../helpers/";
 import { get } from "../../functional";
+import { desktopBreakPoint } from "../../constants";
+import blogBackground from "../../assets/images/blogBackground.jpg";
 
 export class Blog extends Component {
+  state = { desktop: false };
   componentDidMount() {
+    setUpMediaQuery.bind(this)(desktopBreakPoint);
+
     const {
       medium: { expiry }
     } = this.props;
     return shouldFetch(expiry) && this.props.fetchFeed();
   }
+
+  updateMatches = () => {
+    const { matches } = this.mediaQueryList;
+    return this.setState({ desktop: matches });
+  };
+
+  componentWillUnmount() {
+    return this.mediaQueryList.removeListener(this.updateMatches);
+  }
+
   render() {
     const {
       feed: { user, articles }
@@ -21,23 +36,31 @@ export class Blog extends Component {
     const posts = articles
       ? Object.keys(articles).map(id => get(articles, id, []))
       : [];
-    return posts.length > 0 ? (
-      <Container
-        style={{
-          padding: "10px 3px 100px 3px",
-          marginBottom: 50,
-          marginTop: 50
-        }}
-      >
-        <Bio {...user} />
-        <Card.Group>
-          {posts.map(post => (
-            <Feed key={post.id} {...post} />
-          ))}
-        </Card.Group>
-      </Container>
-    ) : (
-      <Loader active inline="centered" />
+
+    const { desktop } = this.state;
+    return (
+      <Fragment>
+        <Background desktop={desktop} background={blogBackground} />
+        <Mask desktop={desktop} tint={0.5} />
+        {posts.length > 0 ? (
+          <Container
+            style={{
+              padding: "10px 3px 100px 3px",
+              marginBottom: 50,
+              marginTop: 50
+            }}
+          >
+            <Bio {...user} />
+            <Card.Group>
+              {posts.map(post => (
+                <Feed key={post.id} {...post} />
+              ))}
+            </Card.Group>
+          </Container>
+        ) : (
+          <Loader active inline="centered" />
+        )}
+      </Fragment>
     );
   }
 }

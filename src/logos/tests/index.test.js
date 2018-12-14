6e938bcdd88c7257b73c803c9c6f3b38f5eec23b
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-14 21:09:30 +0100

    fix: update snapshots

--- src/logos/tests/index.test.js
+++ src/logos/tests/index.test.js
@@ -5,54 +5,8 @@ import BrainStorm from "../BrainStorm";
 import Broken from "../Broken";
 import Experience from "../Experience";
 import Github from "../Github";
-import JavaScriptLogo from "../JavaScriptLogo";
 import Loading from "../Loading";
-import Me from "../Me";
-import Problem from "../Problem";
-import PythonLogo from "../PythonLogo";
-import ReactLogo from "../ReactLogo";
-import Redux from "../Redux";
 import Report from "../Report";
-import Rocket from "../Rocket";
-
-describe("ReactLogo", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<ReactLogo handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
-describe("PythonLogo", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<PythonLogo handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("onClick calls the handler", () => {
-    wrapper.simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
-describe("JavaScriptLogo", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<JavaScriptLogo handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
 
 describe("Award", () => {
   const spy = jest.fn();
@@ -116,49 +70,10 @@ describe("Github", () => {
   });
 });
 
-describe("Me", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<Me handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("Me onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
 describe("Loading", () => {
   const wrapper = shallow(<Loading />);
   it("renders without fill prop", () => {
     expect(wrapper).toMatchSnapshot();
-  });
-});
-
-describe("Problem", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<Problem handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("Problem onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
-describe("Redux", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<Redux handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("Redux onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
-    expect(spy).toHaveBeenCalled();
   });
 });
 
@@ -174,19 +89,6 @@ describe("Report", () => {
       .find("g")
       .at(0)
       .simulate("click");
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
-describe("Rocket", () => {
-  const spy = jest.fn();
-  const wrapper = shallow(<Rocket handler={spy} />);
-  it("renders without fill prop", () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it("Rocket onClick calls the handler", () => {
-    wrapper.find("g").simulate("click");
     expect(spy).toHaveBeenCalled();
   });
 });

533ba7bbf0869538f42b78c9ca4ae473b72a587d
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-01-03 17:00:23 +0100

    style: introduce oceanic next theme

---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+export const baseColors = {
+  dark: "#1B2B34",
+  denseBlue: "#343D46",
+  color2: "#4F5B66",
+  color3: "#65737E",
+  color4: "#A7ADBA",
+  color5: "#C0C5CE",
+  color6: "#CDD3DE",
+  white: "#D8DEE9",
+  red: "#EC5f67",
+  color9: "#F99157",
+  color10: "#FAC863",
+  color11: "#99C794",
+  color12: "#5FB3B3",
+  color13: "#6699CC",
+  color14: "#C594C5",
+  brown: "#AB7967"
+};

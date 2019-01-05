3c32e7aa62ec46750f74dcf211a920859920c94a
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-01-05 19:02:25 +0100

    style: last child margin-bottom on all screens

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,4 +3,14 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex: 1;
+
+  > div > div:last-child {
+    margin-bottom: 15%;
+  }
+
+  @media (max-width: 599px) {
+    > div > div:first-child {
+      margin-top: 10%;
+    }
+  }
 `;

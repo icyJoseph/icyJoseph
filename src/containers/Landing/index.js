60bef498029464528738e817e94e8b13be6ccd93
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-09 23:15:33 +0100

    feat: render languages after counting up commits

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,41 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Stat from "../../components/Stat";
-import { Statistics } from "../../components/Stat/styled";
 import Title from "../../components/Title";
-import withGitHub from "../GitHubHoC";
+import { Statistics } from "../../components/Stat/styled";
+import withGitHub from "../withGitHub";
 
-export const Landing = ({
-  timeLine,
+export function Landing({
   github: {
-    user: { public_gists, public_repos },
-    commits,
+    user: { public_gists = 0, public_repos = 0 },
+    commits = 0,
     languages
-  },
-  desktop
-}) => {
+  }
+}) {
+  const [show, setShow] = useState(false);
+  const onEnd = () => setShow(true);
   return (
     <div>
       <Title>
         <h1>Joseph</h1>
       </Title>
       <Statistics>
-        {languages.map(({ lang, bytes }) => (
-          <Stat key={lang} label={lang} end={bytes} />
-        ))}
-        <Stat label="Commits" end={commits} />
+        <Stat label="Commits" end={commits} onEnd={onEnd} />
         <Stat label="Public Repos" end={public_repos} />
         <Stat label="Gists" end={public_gists} />
       </Statistics>
+      {show && (
+        <Statistics>
+          {languages.map(({ lang, bytes }) => (
+            <Stat key={lang} label={lang} end={bytes} />
+          ))}
+        </Statistics>
+      )}
     </div>
   );
-};
+}
 
 // in the GUI seen as Home
 export default withGitHub(Landing);
 
 Landing.propTypes = {
-  timeLine: PropTypes.array,
-  github: PropTypes.object,
-  desktop: PropTypes.bool
+  github: PropTypes.object
 };

50ec1f9801bc562b80569dfda6be9dd80db8fa6e
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-09 23:14:04 +0100

    refactor: useCount renders only when there is an end value

--- src/components/Stat/index.js
+++ src/components/Stat/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { memo } from "react";
 import { useCountUp } from "react-countup";
 import { StatWrap, StatLabel, StatCount } from "./styled";
 
-export function Stat({ start = 0, end, label }) {
-  const { countUp } = useCountUp({ start, end, duration: 5, delay: 1 });
+export function StatCountUp({
+  end = 0,
+  duration = 5,
+  delay = 1,
+  onEnd = () => {}
+}) {
+  const { countUp } = useCountUp({
+    start: 0,
+    end,
+    duration,
+    delay,
+    onEnd
+  });
+
+  return <StatCount>{countUp}</StatCount>;
+}
+
+export function Stat({ label = "", end, ...rest }) {
   return (
     <StatWrap>
       <StatLabel>{label}</StatLabel>
-      <StatCount>{countUp}</StatCount>
+      {end && <StatCountUp end={end} {...rest} />}
     </StatWrap>
   );
 }
 
-export default Stat;
+export default memo(Stat);

f851bc0719a88ee85c6cd4fcdb6fd8d85d8605d5
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-11 21:59:22 +0100

    style: organize repository card

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,6 +1,122 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHdd } from "@fortawesome/free-solid-svg-icons";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faVuejs } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faUserCheck } from "@fortawesome/free-solid-svg-icons";
+import { faUserTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { RepoWrap } from "./styled";
 import { Tag, TagWrap } from "../Tag";
+
+function ownership(login) {
+  if (login === "icyJoseph") {
+    return faUserCheck;
+  }
+  return faUserTimes;
+}
+
+function languageIcon(lang = "") {
+  const language = lang.toLowerCase();
+  switch (language) {
+    case "javascript":
+      return faJs;
+    case "python":
+      return faPython;
+    case "nodejs":
+      return faNodeJs;
+    case "react":
+      return faReact;
+    case "vuejs":
+      return faVuejs;
+    default:
+      return faLaptopCode;
+  }
+}
+
+function Size({ size }) {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faHdd} />
+      <span className="data-field">
+        <code>{size}kb</code>
+      </span>
+    </div>
+  );
+}
+
+function Language({ language }) {
+  return (
+    <div>
+      <FontAwesomeIcon icon={languageIcon(language)} />
+      <span className="data-field">
+        <code>{language}</code>
+      </span>
+    </div>
+  );
+}
+
+const truncateTxt = str => (str.length > 100 ? `${str.slice(0, 100)}...` : str);
+
+function Description({ description }) {
+  return (
+    <div>
+      <div className="description-field">{truncateTxt(description)}</div>
+    </div>
+  );
+}
+
+function Ownership({ login }) {
+  return (
+    <div className="ownership-field">
+      <span>Owner</span>
+      <FontAwesomeIcon icon={ownership(login)} />
+    </div>
+  );
+}
+
+function RefLinks({ html_url, homepage }) {
+  return (
+    <div className="refs-field">
+      <a href={html_url} target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      {homepage && (
+        <a href={homepage} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faDesktop} />
+        </a>
+      )}
+    </div>
+  );
+}
+
+function daysSince(created_at) {
+  const today = new Date().getTime();
+  const asMs = (today - new Date(created_at).getTime()) / (24 * 60 * 60 * 1000);
+  return Math.floor(asMs);
+}
+
+function Dates({ created_at, pushed_at }) {
+  return (
+    <div className="dates-field">
+      <div>
+        <FontAwesomeIcon icon={faCodeBranch} />
+        <div>created {daysSince(created_at)} days ago</div>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faClock} />
+        <div>pushed {daysSince(pushed_at)} days ago</div>
+      </div>
+    </div>
+  );
+}
 
 export function Repository({
   name,
@@ -17,17 +133,17 @@ export function Repository({
 }) {
   return (
     <RepoWrap>
-      <div>{name}</div>
       <div>
-        <code>{language}</code>
+        <div>{name}</div>
+        <Ownership login={login} />
       </div>
-      <div>{description}</div>
-      <div>Repo url: {html_url}</div>
-      <div>Demo: {homepage}</div>
-      <div>Owner: {login}</div>
-      <div>Last pushed: {pushed_at}</div>
-      <div>Created: {created_at}</div>
-      <div>Size: {size} Kb</div>
+      <div>
+        <Language language={language} />
+        <Size size={size} />
+      </div>
+      <Description description={description} />
+      <RefLinks html_url={html_url} homepage={homepage} />
+      <Dates created_at={created_at} pushed_at={pushed_at} />
       <TagWrap>
         {topics.map(topic => (
           <Tag key={topic}>{topic}</Tag>
--- src/components/Repository/styled.js
+++ src/components/Repository/styled.js
@@ -6,22 +6,24 @@ export const RepoWrap = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
+  justify-content: space-around;
   margin: 0.5em;
-  background: rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.1);
   padding: 0.5em;
   border-radius: 1em;
   ${shadow}
   ${transitionAll}
   font-size: 1em;
   max-width: 300px;
-  min-width: 300px;
+  min-width: 200px;
 
   > div {
-    flex: 1;
+    display: flex;
+    margin: 0.25em 0;
   }
 
-  > div:nth-child(3) {
-    flex: 2;
+  > div:nth-child(1) {
+    flex-direction: column;
   }
 
   > div:first-child {
@@ -30,12 +32,65 @@ export const RepoWrap = styled.div`
   }
 
   > div:nth-child(2) {
+    display: flex;
     font-size: 1.25em;
+    justify-content: space-between;
     color: ${baseColors.subheading};
+    flex-wrap: wrap;
   }
 
   > div:last-child {
-    flex: 3;
     justify-self: flex-end;
+    font-size: 0.65em;
+  }
+
+  .data-field {
+    margin: 0 0.25em;
+  }
+
+  .description-field {
+    margin: 0.5em 0;
+    font-size: 0.9em;
+    color: ${baseColors.light};
+    word-break: break-word;
+  }
+
+  .ownership-field {
+    color: ${baseColors.info};
+    font-size: 0.6em;
+
+    > span {
+      margin: 0 0.25em;
+    }
+  }
+
+  .refs-field {
+    display: flex;
+    font-size: 1.25em;
+
+    > * {
+      height: 100%;
+      color: ${baseColors.success};
+      display: flex;
+      margin-right: 1em;
+    }
+  }
+
+  .dates-field {
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    > * {
+      display: flex;
+      align-items: center;
+      font-size: 0.8em;
+      font-style: italic;
+      color: ${baseColors.default};
+      margin-right: 0.25em;
+    }
+
+    > * > * {
+      margin: 0.15em;
+    }
   }
 `;

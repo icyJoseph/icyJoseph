82a1e0b7fa89b017b8a323f9615b8e99c81f05be
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-05-11 06:51:53 +0200

    style: box shadow belongs to the title css

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,29 +1,25 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { baseColors } from "../../theme";
-
-const titleShadow = css`
-  box-shadow: ${({ shadow }) =>
-    shadow
-      ? `0 0px 16px 1px rgba(255,255,255,0.24), 
-      0 11px 3px -12px rgba(255,255,255,0.4);`
-      : "0px 0px 0px 0px rgba(255,255,255,0);"};
-`;
 
 const StyledTitle = styled.div`
   background: ${baseColors.background};
-  ${titleShadow}
-  min-width: ${({ shadow }) => (shadow ? "150px" : "100px")} ;
+  min-width: ${({ shadow }) => (shadow ? "150px" : "100px")};
   border-radius: 0 0 5px 5px;
   align-self: center;
   transition: all 1s ease;
   z-index: 10;
 
-  
+  box-shadow: ${({ shadow }) =>
+    shadow
+      ? `0 0px 16px 1px rgba(255,255,255,0.24), 
+      0 11px 3px -12px rgba(255,255,255,0.4);`
+      : "0px 0px 0px 0px rgba(255,255,255,0);"};
+
   > div.sticky_sentinel {
     position: absolute;
     left: 0;
-    right: 0; 
+    right: 0;
     visibility: hidden;
   }
 
@@ -32,7 +28,7 @@ const StyledTitle = styled.div`
     top: -30px;
   }
 
-  >div.sticky_sentinel--bottom {
+  > div.sticky_sentinel--bottom {
     bottom: 0;
     height: 80px;
   }
@@ -42,7 +38,7 @@ const StyledTitle = styled.div`
     padding: 0.2em;
     font-size: 1.5em;
     color: ${baseColors.heading};
-    text-align: center;  
+    text-align: center;
   }
 `;
 

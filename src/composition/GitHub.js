09168c17c367bcc5e567cc67fdcd9cdc69acc2de
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-08-27 09:10:30 +0200

    feat: show bio info on GitHub section

--- src/composition/GitHub.js
+++ src/composition/GitHub.js
@@ -2,7 +2,8 @@ import { Flex } from "components/Flex";
 import { Section } from "components/Section";
 import { Text } from "components/Text";
 
-export const GitHub = () => {
+export const GitHub = ({ initial }) => {
+  console.log(initial);
   return (
     <Section my={3} px={2}>
       <header>
@@ -11,15 +12,7 @@ export const GitHub = () => {
         </Text>
       </header>
       <Flex as="main">
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </Text>
+        <Text>{initial.bio}</Text>
       </Flex>
     </Section>
   );

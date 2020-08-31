9e405e5caedd203a94f65e63479cfc3358a975b1
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-08-31 10:55:53 +0200

    feat: useSWR to query /api/github

---
+++ src/hooks/useGitHub.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = (query, variables) =>
+  axios.post("/api/github", { query, variables }).then(({ data }) => data);
+
+export const useGitHub = ({
+  query,
+  variables,
+  initialData = null,
+  selector = (a) => a
+}) => {
+  return useSWR(
+    [query, JSON.stringify(variables)],
+    (...args) => fetcher(...args).then(selector),
+    {
+      shouldRetryOnError: false,
+      revalidateOnFocus: false,
+      initialData
+    }
+  );
+};

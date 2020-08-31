8bcb4362ced7d6ede5daad810c7dff997eac37e9
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-08-31 10:56:13 +0200

    style: Grid Layout for GitHub profile data

--- src/composition/GitHub.js
+++ src/composition/GitHub.js
@@ -1,9 +1,53 @@
+import styled from "styled-components";
 import { Flex } from "components/Flex";
 import { Section } from "components/Section";
 import { Text } from "components/Text";
+import { useGitHub } from "hooks/useGitHub";
+import { GET_USER } from "queries";
+
+const Img = styled.img`
+  align-self: center;
+  justify-self: center;
+  border-radius: 50%;
+  max-width: 192px;
+  min-width: 96px;
+  width: 66.66%;
+`;
+
+const GHGrid = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: minmax(33%, 192px) 1fr;
+    grid-template-rows: auto;
+  }
+`;
+
+const Contributions = styled(Flex)`
+  grid-column: span 2;
+`;
 
 export const GitHub = ({ initial }) => {
-  console.log(initial);
+  const { data } = useGitHub({
+    query: GET_USER,
+    variables: { login: "icyJoseph" },
+    initialData: initial,
+    selector: ({ user }) => user
+  });
+
+  const {
+    bio,
+    name,
+    company,
+    location,
+    login,
+    avatarUrl,
+    contributionsCollection
+  } = data;
+
+  const { contributionYears } = contributionsCollection;
   return (
     <Section my={3} px={2}>
       <header>
@@ -11,9 +55,27 @@ export const GitHub = ({ initial }) => {
           GitHub
         </Text>
       </header>
-      <Flex as="main">
-        <Text>{initial.bio}</Text>
-      </Flex>
+      <GHGrid as="main">
+        <Img src={avatarUrl} alt={`${name} github profile picture`} />
+        <Flex flexDirection="column" py={3} px={2}>
+          <h2>{name}</h2>
+
+          <h3>{login}</h3>
+
+          <Text>{company}</Text>
+
+          <Text>{bio}</Text>
+
+          <Text>{location}</Text>
+        </Flex>
+        <Contributions justifyContent="center">
+          {contributionYears.map((year) => (
+            <Text key={year} as="span" mx={2}>
+              {year}
+            </Text>
+          ))}
+        </Contributions>
+      </GHGrid>
     </Section>
   );
 };

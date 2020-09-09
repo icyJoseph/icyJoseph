536a6979e3eb9f39bdf88670eab6aa3ee1dd5856
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-09 15:41:49 +0200

    feat: yearly contributions query, reusing fragment

--- src/queries.js
+++ src/queries.js
@@ -1,8 +1,48 @@
 import { print } from "graphql";
 import gql from "graphql-tag";
 
+const ContributionsFragment = gql`
+  fragment contributions on ContributionsCollection {
+    startedAt
+    endedAt
+    joinedGitHubContribution {
+      occurredAt
+    }
+    totalCommitContributions
+    restrictedContributionsCount
+    totalRepositoryContributions
+    contributionYears
+    commitContributionsByRepository(maxRepositories: 100) {
+      contributions {
+        totalCount
+      }
+      repository {
+        id
+        name
+        isArchived
+        isDisabled
+        isFork
+        isPrivate
+        diskUsage
+        languages(first: 3) {
+          edges {
+            node {
+              id
+              name
+              color
+            }
+            size
+          }
+          totalSize
+          totalCount
+        }
+      }
+    }
+  }
+`;
+
 const GET_USER_DOC = gql`
-  query getUser($login: String!) {
+  query getUser($login: String!, $from: DateTime, $to: DateTime) {
     rateLimit {
       cost
       limit
@@ -19,41 +59,29 @@ const GET_USER_DOC = gql`
         id
         name
       }
-      contributionsCollection(
-        from: "2019-08-26T17:01:49.963Z"
-        to: "2020-08-26T17:01:49.963Z"
-      ) {
-        totalCommitContributions
-        restrictedContributionsCount
-        contributionYears
-        commitContributionsByRepository(maxRepositories: 10) {
-          contributions {
-            totalCount
-          }
-          repository {
-            id
-            name
-            isArchived
-            isDisabled
-            isFork
-            isPrivate
-            languages(first: 10) {
-              edges {
-                node {
-                  id
-                  name
-                  color
-                }
-                size
-              }
-              totalSize
-              totalCount
-            }
-          }
-        }
+      contributionsCollection(from: $from, to: $to) {
+        ...contributions
       }
     }
   }
+  ${ContributionsFragment}
+`;
+
+const GET_YEAR_CONTRIBUTIONS_DOC = gql`
+  query getYearContributions($login: String!, $from: DateTime, $to: DateTime) {
+    rateLimit {
+      cost
+      limit
+      remaining
+    }
+    user(login: $login) {
+      contributionsCollection(from: $from, to: $to) {
+        ...contributions
+      }
+    }
+  }
+  ${ContributionsFragment}
 `;
 
 export const GET_USER = print(GET_USER_DOC);
+export const GET_YEAR_CONTRIBUTIONS = print(GET_YEAR_CONTRIBUTIONS_DOC);

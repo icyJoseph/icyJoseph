a500b40b2b356532fe3c8c80506f48617d0be805
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-09 15:30:46 +0200

    refactor: return the complete result of useSWR

--- src/hooks/useCodeWars.js
+++ src/hooks/useCodeWars.js
@@ -4,12 +4,10 @@ import useSWR from "swr";
 const fetcher = () => axios.get("/api/codewars").then(({ data }) => data);
 
 export const useCodeWars = (initialData = null) => {
-  const { data, loading, error } = useSWR("code-wars", fetcher, {
+  return useSWR("code-wars", fetcher, {
     shouldRetryOnError: false,
     initialData
   });
-
-  return { data, loading, error };
 };
 
 export const RenderWithCodeWars = ({ initial, children }) => {

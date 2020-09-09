1edd2d12cc70815396629070bdd8838f9c47be09
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-09 15:39:20 +0200

    feat: ISO date creators compatible with GitHubs DateTime Scalar

--- src/helpers.js
+++ src/helpers.js
@@ -17,3 +17,20 @@ export function onScrollThreshold(setter, threshold = 100) {
   }
   return setter(false);
 }
+
+// from today at midnight till january 1st at midnight
+export const yearStart = (year = new Date().getFullYear()) => {
+  const from = new Date(`${year}-01-01`);
+  from.setUTCHours(0, 0, 0, 0);
+
+  return { from: from.toISOString() };
+};
+
+// a whole year span
+export const yearEnd = (year) => {
+  const to = new Date(`${year}-12-31`);
+  to.setUTCHours(23, 59, 59, 999);
+  const from = new Date(`${year}-01-01`);
+  from.setUTCHours(0, 0, 0, 0);
+  return { from: from.toISOString(), to: to.toISOString() };
+};

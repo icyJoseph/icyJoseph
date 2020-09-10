a38d1363b7c6004a209e841a52685102e97beb90
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-10 11:26:37 +0200

    style: use sans-serif font

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,11 +1,11 @@
 import { css } from "styled-components";
 
 export const typography = css`
-  @font-face {
+  /* @font-face {
     font-family: "Recursive";
     font-display: swap;
     src: url(/fonts/recursive.ttf) format("truetype");
-  }
+  } */
 
   html {
     font-size: 0.625rem;

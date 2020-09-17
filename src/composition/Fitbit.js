523644357c8ea7808801c17eec8b83e01df95b07
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-17 15:58:35 +0200

    feat: inject initial Fitbit data for pre-rendering and render an Activity section

---
+++ src/composition/Fitbit.js
@@ -0,0 +1,63 @@
+import { Box } from "components/Box";
+import { Card } from "components/Card";
+import { Emoji } from "components/Emoji";
+import { DataEntry } from "components/DataEntry";
+import { Flex } from "components/Flex";
+import { Section } from "components/Section";
+import { Text } from "components/Text";
+
+import { useFitbitHR } from "hooks/useFitbit";
+
+export const Fitbit = ({ profile }) => {
+  const { data } = useFitbitHR({ date: "today", period: "1m" });
+
+  const heartData = data?.["activities-heart"] ?? [];
+
+  const [lastDay = null] = heartData.slice(-1);
+  return (
+    <Section my={3} px={2}>
+      <header id="activity">
+        <Text as="h2" color="--blue" fontSize="2rem">
+          <a href="#activity">
+            <code>Activity</code>
+          </a>
+        </Text>
+      </header>
+      <Box as="main">
+        <Box my={2}>
+          <Text>Joined Fitbit in {profile.memberSince}</Text>
+        </Box>
+        <Flex>
+          <Card my={2} mx="auto">
+            <Card.Header>
+              <h2>Steps</h2>
+            </Card.Header>
+            <Card.Section>
+              <DataEntry>
+                <Text color="--smokeyWhite">
+                  Avg Daily Steps: {profile.averageDailySteps}
+                </Text>
+                <Text color="--smokeyWhite">
+                  Resting pulse today: {lastDay?.value?.restingHeartRate}
+                </Text>
+              </DataEntry>
+            </Card.Section>
+          </Card>
+          {profile.topBadges.map((badge) => (
+            <Card my={2} mx="auto" key={badge.encodedId}>
+              <Card.Header>{badge.shortName}</Card.Header>
+              <Card.Section>
+                <DataEntry>
+                  <Text color="--smokeyWhite">{badge.description}</Text>
+                  <Text color="--smokeyWhite">
+                    Achived: {badge.timesAchieved} times
+                  </Text>
+                </DataEntry>
+              </Card.Section>
+            </Card>
+          ))}
+        </Flex>
+      </Box>
+    </Section>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,22 @@ import { promisify } from "util";
 
 import Head from "next/head";
 
-import { CodeWars } from "composition/CodeWars";
 import { Container } from "components/Container";
+
+import { CodeWars } from "composition/CodeWars";
+import { Fitbit } from "composition/Fitbit";
 import { Tokei } from "composition/Tokei";
 import { GitHub } from "composition/GitHub";
 
 import { getCodeWarsUser } from "pages/api/codewars";
 import { queryGitHub } from "pages/api/github";
+import { fitbitAuth } from "pages/api/fitbit/profile";
 
 import { GET_USER } from "queries";
 
 import { yearStart } from "helpers";
 
-export function Home({ codewars, github, tokei }) {
+export function Home({ codewars, github, tokei, fitbit }) {
   return (
     <>
       <Head>
@@ -27,6 +30,7 @@ export function Home({ codewars, github, tokei }) {
         <Tokei tokei={tokei} />
         <CodeWars initial={codewars} />
         <GitHub initial={github} />
+        <Fitbit profile={fitbit} />
       </Container>
     </>
   );
@@ -45,7 +49,11 @@ export async function getStaticProps() {
     "utf-8"
   ).then(JSON.parse);
 
-  return { props: { codewars, github, tokei } };
+  const fitbit = await fitbitAuth
+    .get("/profile.json")
+    .then(({ data }) => data.user);
+
+  return { props: { codewars, github, tokei, fitbit } };
 }
 
 export default Home;

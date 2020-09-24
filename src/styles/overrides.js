a665b530b921d122371d522ad837462388242338
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-09-24 15:19:20 +0200

    chore: apply Prettier on overrides

--- src/styles/overrides.js
+++ src/styles/overrides.js
@@ -1,7 +1,7 @@
-import {css} from 'styled-components';
+import { css } from "styled-components";
 
 export const overrides = css`
-    .devicon-rust-plain.colored {
-        color: #dea584;
-    }
-`
+  .devicon-rust-plain.colored {
+    color: #dea584;
+  }
+`;

123d1eb836b8bb9a94559af2dc75f8be4bff8bd6
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-10-01 13:25:40 +0200

    feat: create a detached fetcher and fix pagination

--- src/pages/api/fitbit/activities/list.js
+++ src/pages/api/fitbit/activities/list.js
@@ -1,24 +1,31 @@
 import { fitbitAuth } from "pages/api/fitbit/profile";
 
+export const getActivityLog = async (params) => {
+  const query = encodeURI(
+    Object.entries({ ...params, sort: "asc", offset: 0, limit: 20 })
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&")
+  );
+
+  const { data } = await fitbitAuth.get(`/activities/list.json?${query}`);
+  const aggregate = await paginate(data);
+
+  return aggregate;
+};
+
 const paginate = async ({ activities, pagination }) => {
   const { data } = await fitbitAuth.get(pagination.next);
 
   const aggregate = [...activities, ...data.activities];
-  if (data.next) {
+
+  if (data.pagination.next) {
     return await paginate({ ...data, activities: aggregate });
   }
   return aggregate;
 };
 
 export default async (req, res) => {
-  const query = encodeURI(
-    Object.entries(req.query)
-      .map(([key, value]) => `${key}=${value}`)
-      .join("&")
-  );
+  const activityLog = await getActivityLog(req.query);
 
-  const { data } = await fitbitAuth.get(`/activities/list.json?${query}`);
-
-  const aggregate = await paginate(data);
-  return res.send(aggregate);
+  return res.send(activityLog);
 };

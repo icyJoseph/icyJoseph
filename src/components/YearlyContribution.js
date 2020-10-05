0da0a74fc0131b9fe1c6861aa6adb0d6a290e4bb
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-10-05 22:57:20 +0200

    style: simplify repo view, less clutter animations

--- src/components/YearlyContribution.js
+++ src/components/YearlyContribution.js
@@ -1,7 +1,5 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import styled from "styled-components";
-
-import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import styled, { css } from "styled-components";
 
 import { Box } from "components/Box";
 import { Button } from "components/Button";
@@ -19,39 +17,25 @@ import { clamp } from "helpers";
 
 const cardWidth = 250;
 
-const ContributionsSummary = styled(Flex)`
-  grid-column: span 2;
+const staleMixin = css`
   opacity: ${({ stale = false }) => (stale ? 0.5 : 1)};
   transition: opacity 0.5s ease-in-out;
 `;
 
-const RepoProgress = styled(Box)`
+const ContributionsSummary = styled(Flex)`
   grid-column: span 2;
-  text-align: center;
+  ${staleMixin};
 `;
 
 const RepositoriesWithOptions = styled(Box)`
   grid-column: span 2;
+  scroll-behavior: smooth;
+  ${staleMixin};
 `;
 
-const RepositoriesGrid = styled(motion.div)`
+const RepositoriesGrid = styled.div`
   display: flex;
-  flex-direction: column;
-
-  justify-content: unset;
-  align-items: center;
-
-  flex-wrap: wrap;
-
-  grid-column: span 2;
-  grid-template-columns: repeat(auto-fit, 1fr);
-  grid-template-rows: auto;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: center;
-    align-items: unset;
-  }
+  justify-content: center;
 `;
 
 const LanguageName = styled(Text)`
@@ -74,9 +58,6 @@ const Indicator = styled.div`
 const Options = styled.div`
   display: flex;
   grid-column: span 3;
-  position: sticky;
-  bottom: 5px;
-  z-index: 10;
 `;
 
 const OptionButton = styled(Button)`
@@ -87,7 +68,7 @@ const OptionButton = styled(Button)`
 
 const StyledCard = styled(Card)`
   width: 200px;
-  
+
   @media (min-width: 320px) {
     width: ${cardWidth}px;
   }
@@ -111,10 +92,10 @@ export const YearlyContribution = ({ initial, year, from, to }) => {
 
   const [windowSize, setWindowSize] = useState(1);
 
-  const [pointer, setPointer] = useState(windowSize);
+  const [pointer, setPointer] = useState(0);
 
   useEffect(() => {
-    setPointer(windowSize);
+    setPointer(0);
   }, [year]);
 
   const prev = useLastNonNullableValue(data);
@@ -142,13 +123,6 @@ export const YearlyContribution = ({ initial, year, from, to }) => {
   );
 
   const ref = useRef(null);
-
-  useEffect(() => {
-    setPointer((prev) => {
-      const coef = Math.ceil(prev / windowSize);
-      return coef * windowSize;
-    });
-  }, [windowSize]);
 
   useLayoutEffect(() => {
     const handler = () => {
@@ -191,141 +165,99 @@ export const YearlyContribution = ({ initial, year, from, to }) => {
           </Text>
         </Box>
       </ContributionsSummary>
-      <AnimateSharedLayout>
-        {!stale && (
-          <React.Fragment>
-            <RepoProgress>
-              {!!commitContributionsByRepository.length && (
-                <span>
-                  {pointer} / {commitContributionsByRepository.length}
-                </span>
-              )}
-            </RepoProgress>
-            <RepositoriesWithOptions>
-              <RepositoriesGrid layout m={2} ref={ref}>
-                <AnimatePresence initial={false}>
-                  {commitContributionsByRepository
-                    .slice(0, pointer)
-                    .reverse()
-                    .map(({ contributions, repository }) => (
-                      <motion.div
-                        key={`${repository.id}-${year}`}
-                        initial={{
-                          height: 0,
-                          opacity: 0
-                        }}
-                        animate={{ height: "auto", opacity: 1 }}
-                        transition={{
-                          type: "spring",
-                          stiffness: 400,
-                          damping: 20
-                        }}
-                        exit={{
-                          height: 0,
-                          opacity: 0
-                        }}
-                      >
-                        <StyledCard p={2} m={2}>
-                          <Card.Header>
-                            <Text
-                              as="h4"
-                              color="--smokeyWhite"
-                              fontSize="2rem"
-                              fontWeight={600}
-                              mb={2}
-                            >
-                              {repository.name}
-                            </Text>
-                            <Text color="--smokeyWhite">Owner:</Text>
-                            <Text color="--smokeyWhite">
-                              {repository.owner.login}
-                            </Text>
-                          </Card.Header>
-                          <Card.Section>
-                            <Text color="--smokeyWhite">
-                              Contributions: {contributions.totalCount}
-                            </Text>
-                            <Text color="--smokeyWhite">
-                              Size: {repository.languages.totalSize} bytes
-                            </Text>
-                          </Card.Section>
-                          <Card.Section>
-                            {!repository.isArchived &&
-                              repository?.languages?.edges.map(
-                                ({ node: { color, name }, size }) => (
-                                  <Box key={name} mt={2}>
-                                    <LanguageName mb={1}>
-                                      {name}: {size} bytes
-                                    </LanguageName>
-                                    <DevIcon
-                                      colored
-                                      language={name}
-                                      mb={2}
-                                      fontSize="1.75rem"
-                                    />
-                                    <Indicator
-                                      color={color}
-                                      percentage={
-                                        (100 * size) /
-                                        repository.languages.totalSize
-                                      }
-                                    />
-                                  </Box>
-                                )
-                              )}
-                          </Card.Section>
-                        </StyledCard>
-                      </motion.div>
-                    ))}
-                </AnimatePresence>
-              </RepositoriesGrid>
-              <Options>
-                <OptionButton
-                  text={`Less`}
-                  onClick={() => {
-                    setPointer((x) =>
-                      clamp(
-                        x - windowSize,
-                        0,
-                        commitContributionsByRepository.length
+
+      <RepositoriesWithOptions stale={stale}>
+        <Options>
+          <OptionButton
+            text={`Prev`}
+            onClick={() => {
+              setPointer((x) =>
+                clamp(x - windowSize, 0, commitContributionsByRepository.length)
+              );
+            }}
+            disabled={pointer === 0}
+            my={2}
+            mx="auto"
+          />
+          <OptionButton
+            text={`Next`}
+            onClick={() => {
+              setPointer((x) =>
+                clamp(x + windowSize, 0, commitContributionsByRepository.length)
+              );
+            }}
+            disabled={
+              pointer + windowSize >= commitContributionsByRepository.length
+            }
+            my={2}
+            mx="auto"
+          />
+        </Options>
+        <RepositoriesGrid m={2} ref={ref}>
+          {commitContributionsByRepository
+            .slice(pointer, pointer + windowSize)
+            .map(({ contributions, repository }, index) => (
+              <StyledCard key={repository.id} p={2} m={2}>
+                <Card.Header>
+                  <Flex justifyContent="space-between" flexWrap="nowrap">
+                    <Text
+                      as="h4"
+                      color="--smokeyWhite"
+                      fontSize="2rem"
+                      fontWeight={600}
+                      mb={2}
+                    >
+                      {repository.name}
+                    </Text>
+                    <Text as="span" color="--yellow">
+                      #{pointer + index + 1}
+                    </Text>
+                  </Flex>
+                  <Text color="--smokeyWhite">Owner:</Text>
+                  <Text color="--smokeyWhite">{repository.owner.login}</Text>
+                </Card.Header>
+                <Card.Section>
+                  <Text color="--smokeyWhite">
+                    Contributions: {contributions.totalCount}
+                  </Text>
+                  <Text color="--smokeyWhite">
+                    Size: {repository.languages.totalSize} bytes
+                  </Text>
+                </Card.Section>
+                <Card.Section>
+                  {!repository.isArchived &&
+                    repository?.languages?.edges.map(
+                      ({ node: { color, name }, size }) => (
+                        <Box key={name} mt={2}>
+                          <LanguageName mb={1}>
+                            {name}: {size} bytes
+                          </LanguageName>
+                          <DevIcon
+                            colored
+                            language={name}
+                            mb={2}
+                            fontSize="1.75rem"
+                          />
+                          <Indicator
+                            color={color}
+                            percentage={
+                              (100 * size) / repository.languages.totalSize
+                            }
+                          />
+                        </Box>
                       )
-                    );
-                  }}
-                  disabled={pointer <= windowSize}
-                  my={2}
-                  mx="auto"
-                />
-                <OptionButton
-                  text={`More`}
-                  onClick={() => {
-                    setPointer((x) =>
-                      clamp(
-                        x + windowSize,
-                        0,
-                        commitContributionsByRepository.length
-                      )
-                    );
-                  }}
-                  disabled={pointer >= commitContributionsByRepository.length}
-                  my={2}
-                  mx="auto"
-                />
-              </Options>
-            </RepositoriesWithOptions>
-            {joinedGitHubContribution && (
-              <ContributionsSummary my={2} mx="auto">
-                <Text>Joined GitHub</Text>
-                <Emoji
-                  symbol="🎉"
-                  title="Joined Github"
-                  ariaLabel="Tada"
-                  mx={2}
-                />
-              </ContributionsSummary>
-            )}
-          </React.Fragment>
-        )}
-      </AnimateSharedLayout>
+                    )}
+                </Card.Section>
+              </StyledCard>
+            ))}
+        </RepositoriesGrid>
+      </RepositoriesWithOptions>
+      {joinedGitHubContribution && (
+        <ContributionsSummary my={2} mx="auto">
+          <Text fontSize="2rem">Joined GitHub</Text>
+          <Emoji symbol="🎉" title="Joined Github" ariaLabel="Tada" mx={2} />
+        </ContributionsSummary>
+      )}
     </>
   );
 };

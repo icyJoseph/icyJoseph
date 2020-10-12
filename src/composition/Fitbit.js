1c19cae6d409d6192431ac41644937cbc600cea8
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-10-12 19:30:43 +0200

    feat: show 1 month summary of heart rate zones

--- src/composition/Fitbit.js
+++ src/composition/Fitbit.js
@@ -11,39 +11,131 @@ import { ActivityLog } from "components/Fitbit/ActivityLog";
 
 import { useFitbitHR } from "hooks/useFitbit";
 
-export const Fitbit = ({ profile, activityLog }) => {
-  const { data } = useFitbitHR({ date: "today", period: "1m" });
+const StyledDataEntry = styled(DataEntry)`
+  flex-basis: 33.33%;
+
+  & > p:not(:first-child) {
+    text-transform: inherit;
+  }
+`;
+
+const heartRateMonthSummary = (points) =>
+  points.reduce((prev, { value }) => {
+    const { heartRateZones } = value;
+
+    heartRateZones.forEach(({ name, ...rest }) => {
+      if (prev.has(name)) {
+        const current = prev.get(name);
+
+        const update = {
+          ...current,
+          ...rest,
+          caloriesOut: current.caloriesOut + (rest.caloriesOut ?? 0),
+          minutes: current.minutes + (rest.minutes ?? 0)
+        };
+        prev.set(name, update);
+      } else {
+        prev.set(name, { name, ...rest });
+      }
+    });
+
+    return prev;
+  }, new Map());
+
+export const Fitbit = ({ profile, activityLog, initialHR }) => {
+  const { data } = useFitbitHR(
+    { date: "today", period: "1m", revalidateOnMount: true },
+    initialHR
+  );
 
   const heartData = data?.["activities-heart"] ?? [];
-  const [lastDay = null] = heartData.slice(-1);
+  const [prevDay = null, today = null] = heartData.slice(-2);
+  const summary = heartRateMonthSummary(heartData);
 
   return (
     <Section my={3} px={2}>
-      <header id="activity">
+      <header id="fitbit">
         <Text as="h2" color="--blue" fontSize="3rem">
-          <a href="#activity">
-            <code>Activity</code>
+          <a href="#fitbit">
+            <code>Fitbit</code>
           </a>
         </Text>
       </header>
       <Box as="main">
-        <Text as="h6" fontSize="1.8rem" my={2}>
-          Joined Fitbit in {profile.memberSince}
-        </Text>
         <Flex flexDirection="column" my={2}>
           <InfoCard mx="auto">
             <Card.Header>
-              <h2>Steps</h2>
+              <h2>Summary</h2>
+              <Text
+                as="h6"
+                fontSize="1.5rem"
+                my={2}
+                color="--smokeyWhite"
+                fontWeight={300}
+              >
+                Joined Fitbit in {profile.memberSince}
+              </Text>
             </Card.Header>
             <Card.Section>
-              <DataEntry>
-                <Text color="--smokeyWhite">
-                  Avg Daily Steps: {profile.averageDailySteps}
+              <Flex flex={1} flexDirection="column" alignItems="center">
+                <Text color="--smokeyWhite">Avg Daily Steps:</Text>
+
+                <Text color="--yellow" fontWeight="bold" m={3} fontSize="2rem">
+                  {profile.averageDailySteps}
                 </Text>
+
                 <Text color="--smokeyWhite">
-                  Resting pulse today: {lastDay?.value?.restingHeartRate}
+                  Resting pulse{" "}
+                  <Text
+                    as="span"
+                    textTransform="lowercase"
+                    color="--smokeyWhite"
+                  >
+                    (bpm)
+                  </Text>
+                  :
                 </Text>
-              </DataEntry>
+                <Text color="--yellow" fontWeight="bold" m={3} fontSize="2rem">
+                  {today?.value?.restingHeartRate ??
+                    prevDay?.value?.restingHeartRate}
+                </Text>
+
+                <Text color="--smokeyWhite">Last {heartData.length} days</Text>
+
+                <Flex>
+                  {Array.from(summary.values()).map(
+                    ({ name, minutes, caloriesOut }) => {
+                      return (
+                        <StyledDataEntry key={name}>
+                          <Text color="--smokeyWhite" fontWeight={300}>
+                            {name}
+                          </Text>
+                          <Text color="--yellow" fontWeight="bold" mt={2}>
+                            {minutes}{" "}
+                            <Text
+                              as="span"
+                              color="--smokeyWhite"
+                              fontWeight={300}
+                            >
+                              min
+                            </Text>
+                          </Text>
+                          <Text color="--yellow" fontWeight="bold" mt={2}>
+                            {Math.floor(caloriesOut)}{" "}
+                            <Text
+                              as="span"
+                              color="--smokeyWhite"
+                              fontWeight={300}
+                            >
+                              Cal
+                            </Text>
+                          </Text>
+                        </StyledDataEntry>
+                      );
+                    }
+                  )}
+                </Flex>
+              </Flex>
             </Card.Section>
           </InfoCard>
           <TopBadges profile={profile} />

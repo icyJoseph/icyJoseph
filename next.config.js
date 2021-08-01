4adbe964e1713b3076d0086c3b83003109461432
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-08-01 22:18:18 +0200

    chore: Remove basePath option from next config

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const withSourceMaps = require("@zeit/next-source-maps");
 module.exports = withSourceMaps(
   withBundleAnalyzer({
     poweredByHeader: false,
-    basePath: "",
-    // The starter code load resources from `public` folder with `router.basePath` in React components.
-    // So, the source code is "basePath-ready".
-    // You can remove `basePath` if you don't need it.
     reactStrictMode: true,
     webpack(config) {
       return config;

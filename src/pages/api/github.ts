b60031ce27959e6d39de6aa9105344169b11d523
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-11-12 15:04:10 +0100

    feat: Sunset contact form

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -1,11 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 const githubAuth = axios.create({
   baseURL: "https://api.github.com/",
-  auth: { username: "icyJoseph", password: process.env.GITHUB_TOKEN }
+  auth: { username: "icyJoseph", password: process.env.GITHUB_TOKEN || "" }
 });
 
-export const queryGitHub = (query, variables) =>
+export const queryGitHub = (query: string, variables: Record<string, string>) =>
   githubAuth
     .post("graphql", {
       query,
@@ -13,7 +14,7 @@ export const queryGitHub = (query, variables) =>
     })
     .then(({ data }) => data);
 
-const github = async (req, res) => {
+const github = async (req: NextApiRequest, res: NextApiResponse) => {
   const { data } = await queryGitHub(req.body.query, req.body.variables);
 
   return res.send(data);

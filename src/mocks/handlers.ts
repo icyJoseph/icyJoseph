13b4b1bd720506c83436ee38d39ae8ac771c7ec4
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-11-15 12:03:10 +0100

    test: Include MSW with fitbit mocks

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,27 @@
+import { rest } from "msw";
+import profileJSON from "./data/fitbit/profile.json";
+import heartRateJSON from "./data/fitbit/heart-activity.json";
+import listJSON from "./data/fitbit/list.json";
+
+const fitbitProfile = rest.get(
+  "https://api.fitbit.com/1/user/-/profile.json",
+  (req, res, ctx) => {
+    return res(ctx.json(profileJSON));
+  }
+);
+
+const fitbitHeartRate = rest.get(
+  "https://api.fitbit.com/1/user/-/activities/heart/date/today/1m.json",
+  (req, res, ctx) => {
+    return res(ctx.json(heartRateJSON));
+  }
+);
+
+const fitbitList = rest.get(
+  "https://api.fitbit.com/1/user/-/activities/list.json",
+  (req, res, ctx) => {
+    return res(ctx.json(listJSON));
+  }
+);
+
+export const handlers = [fitbitProfile, fitbitHeartRate, fitbitList];

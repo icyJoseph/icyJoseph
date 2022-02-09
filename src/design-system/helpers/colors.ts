a5b629e5467182b3cc0fd77d4b35d3a6feff9643
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-02-09 23:19:38 +0100

    fix: Colors helper with type guard

--- src/design-system/helpers/colors.ts
+++ src/design-system/helpers/colors.ts
@@ -1,8 +1,12 @@
 import { css } from "styled-components";
 
-import { ColorVars, theme } from "design-system/Global/theme";
+import {
+  type ColorVars,
+  theme,
+  toThemeColor
+} from "design-system/Global/theme";
 
 export const color = css<{ $color: ColorVars }>`
-  color: ${({ $color }) => theme[$color]};
+  color: ${({ $color }) => theme[toThemeColor($color)]};
   color: ${({ $color }) => `var(${$color})`};
 `;

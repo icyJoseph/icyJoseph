b6eb7878be70ea90771f3ad46864008079d02d08
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-03-12 16:15:20 +0100

    chore: Remove unusued testing support files

--- jest.config.js
+++ jest.config.js
@@ -5,39 +5,40 @@ const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./"
+  dir: "./",
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  modulePaths: ["<rootDir>/src"],
+
+  coverageReporters: ["text", "lcov"],
   collectCoverageFrom: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
-    "!**/node_modules/**"
+    "!**/node_modules/**",
   ],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  transform: {
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js"
-  },
-  transformIgnorePatterns: [
-    "/node_modules/",
-    "^.+\\.module\\.(css|sass|scss)$"
-  ],
-  moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
-  },
-  modulePaths: ["<rootDir>/src"],
-  coverageReporters: ["text", "lcov"],
   coverageThreshold: {
     global: {
       branches: 75,
       functions: 75,
       lines: 75,
-      statements: 75
-    }
-  }
+      statements: 75,
+    },
+  },
+
+  transform: {
+    "<rootDir>/node_modules/next-mdx-remote/serialize.js": [
+      "@swc/jest",
+      {
+        module: { type: "es6" },
+      },
+    ],
+  },
+
+  transformIgnorePatterns: ["/node_modules/next-mdx-remote"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async

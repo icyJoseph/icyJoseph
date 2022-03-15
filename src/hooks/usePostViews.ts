cc6a51a6ef5ee58a44134afc1b117d2a4e5a3385
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-03-15 14:51:07 +0100

    feat: Use post views

---
+++ src/hooks/usePostViews.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { isRecord, visits } from "count-api";
+
+export const usePostViews = (slug: string) => {
+  const [views, setViews] = useState<number | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    visits(slug, controller.signal)
+      .then(({ json }) => {
+        if (isRecord(json)) {
+          const { value } = json;
+
+          if (typeof value !== "number") return;
+
+          setViews(Number(value));
+        }
+      })
+      .catch(() => {
+        console.warn("Error while getting post views");
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [slug]);
+
+  return views;
+};

e94b4e105092e42f9d717d094ddb070bfd808900
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-04-28 12:56:05 +0200

    feat: Landing page paragraphs with icons

--- src/composition/Introduction.tsx
+++ src/composition/Introduction.tsx
@@ -1,10 +1,39 @@
-import { useId } from "react";
+import { type ReactNode, type FC, useId } from "react";
 
+import {
+  type IconProps,
+  Bird,
+  Briefcase,
+  CodeBlock,
+  FileRs,
+  Student,
+} from "@phosphor-icons/react";
 import classNames from "classnames";
 
 import { Bold } from "design-system/Bold";
 
-const paragraph = "font-sans mx-auto my-8";
+const ParagraphWithIcon = ({
+  className = "",
+  Icon,
+  children,
+}: {
+  className?: string;
+  Icon: FC<IconProps>;
+  children: ReactNode;
+}) => (
+  <div
+    className={classNames(
+      "font-sans flex flex-col sm:flex-row mx-auto my-12 gap-6",
+      className
+    )}
+  >
+    <div className="flex justify-center items-center">
+      <Icon size={32} />
+    </div>
+
+    <p>{children}</p>
+  </div>
+);
 
 export const Introduction = () => {
   const h1HeadingId = useId();
@@ -44,36 +73,36 @@ export const Introduction = () => {
       </p>
 
       <div className="font-mono max-w-[55ch] text-[2rem] font-light mx-auto">
-        <p className={classNames(paragraph, "mt-16")}>
+        <ParagraphWithIcon Icon={Student} className="mt-16">
           Developer from <Bold>Peru</Bold>, living in <Bold>Sweden</Bold>. I
           have a Bachelor&apos;s in Electronics Engineering, and a Master&apos;s
           degree on business design.
-        </p>
+        </ParagraphWithIcon>
 
-        <p className={paragraph}>
+        <ParagraphWithIcon Icon={Briefcase}>
           I have over{" "}
           <Bold title="I do not believe in the value of this number.">
             {new Date().getFullYear() - 2016} years
           </Bold>{" "}
           of experience working as a software developer, in telecom, mining,
           freight, real state, news, transport and automotive industries.
-        </p>
+        </ParagraphWithIcon>
 
-        <p className={paragraph}>
+        <ParagraphWithIcon Icon={CodeBlock}>
           I write <Bold>JavaScript</Bold>, and <Bold>TypeScript</Bold>. I am
           comfortable with any runtime, browser and <Bold>NodeJS</Bold>, even a
           little <Bold>Deno</Bold>.
-        </p>
+        </ParagraphWithIcon>
 
-        <p className={paragraph}>
+        <ParagraphWithIcon Icon={FileRs}>
           Learning <Bold>Rust</Bold> on my free time, in fact it is my go to
           language for coding challenges.
-        </p>
+        </ParagraphWithIcon>
 
-        <p className={paragraph}>
+        <ParagraphWithIcon Icon={Bird}>
           I have started to learn <Bold>Swift</Bold>, mainly by building iOS
           apps on xcode, and occasionally using it to solve coding challenges.
-        </p>
+        </ParagraphWithIcon>
       </div>
     </section>
   );

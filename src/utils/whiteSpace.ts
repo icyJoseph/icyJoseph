30c2e8588c6d46a21bd52562c86a24c41c2bd2f7
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-05-12 14:57:57 +0200

    chore: Delete unused APIs, code helpers, utils

--- src/utils/whiteSpace.ts
+++
@@ -1,5 +0,0 @@
-export const whiteSpaceValidator = (value: string) => {
-  const val = value ?? "";
-  if (val.trim().length === val.length) return true;
-  return "No surrouding white spaces.";
-};

0d57a8655d0e9cdf5c8137a12999ac09e3adc20f
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-12-11 21:23:37 +0100

    feat: Zod post parser

---
+++ src/lib/posts/types.ts
@@ -0,0 +1,25 @@
+export type Post = {
+  // Unchangeable publication date
+  publish_date: number | null;
+  // Last time post was edited
+  update_date?: number | null;
+  // Post title as seen by the reader
+  title: string;
+  // Post human readable resource name
+  slug: string;
+  // To be used when previewing the post
+  summary: string;
+  // Post content
+  content: string | null;
+  // Optional image to use for previews
+  image?: string | null;
+  // What subjects is the post about
+  tags: Array<string>;
+  // Who wrote the post
+  authors: Array<string>;
+};
+
+export type PostPreview = Pick<
+  Post,
+  "title" | "tags" | "slug" | "summary" | "publish_date"
+>;

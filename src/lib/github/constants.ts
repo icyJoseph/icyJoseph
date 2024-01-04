7e926d76f8d51b5efb27de29ec41cc6a789a65fe
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-01-04 11:04:29 +0100

    feat: joined and zero contribution card as GitHub constants

--- src/lib/github/constants.ts
+++ src/lib/github/constants.ts
@@ -1,1 +1,61 @@
 export const ICY_JOSEPH = "icyJoseph";
+
+export const joinedGitHubContribution = {
+  id: "joined-github-contribution",
+  index: -1,
+  repository: {
+    id: "joined-github-contribution",
+    name: "Joined GitHub",
+    url: "",
+    homepageUrl: "https://icyjoseph.dev/",
+    languages: {
+      edges: [],
+      totalCount: 0,
+      totalSize: 0,
+    },
+    description: `
+      @icyJoseph joins GitHub 🎉
+      `,
+    owner: {
+      login: "icyJoseph",
+    },
+    isArchived: false,
+    isDisabled: false,
+    isFork: false,
+    isPrivate: false,
+    diskUsage: 0,
+  },
+  contributions: {
+    totalCount: 0,
+  },
+};
+
+export const zeroContributions = {
+  id: "zero-github-contribution",
+  index: -1,
+  repository: {
+    id: "zero-github-contribution",
+    name: "New Year",
+    url: "",
+    homepageUrl: "https://icyjoseph.dev/",
+    languages: {
+      edges: [],
+      totalCount: 0,
+      totalSize: 0,
+    },
+    description: `
+        Not contributions, yet.
+        `,
+    owner: {
+      login: "icyJoseph",
+    },
+    isArchived: false,
+    isDisabled: false,
+    isFork: false,
+    isPrivate: false,
+    diskUsage: 0,
+  },
+  contributions: {
+    totalCount: 0,
+  },
+};

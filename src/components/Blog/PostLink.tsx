119d9f510ff3a40d4bd61092ba0eb7e58166159f
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-07-16 22:23:10 +0200

    style: Semantic card link

--- src/components/Blog/PostLink.tsx
+++ src/components/Blog/PostLink.tsx
@@ -11,13 +11,17 @@ export const PostLink = ({
   post: PostPreview;
   children?: ReactNode | ReactNode[];
 }) => (
-  <Link key={post.slug} href={`/blog/${post.slug}`}>
-    <article className="mb-8 px-1">
+  <article className="mb-8 px-1 relative">
+    <Link
+      key={post.slug}
+      href={`/blog/${post.slug}`}
+      className="hover:underline decoration-pale-yellow after:absolute after:content-[''] after:top-0 after:left-0 after:bottom-0 after:right-0 after:inset-0"
+    >
       <h3 className="text-pale-yellow text-xl">{post.title}</h3>
+    </Link>
 
-      <p className="font-sans text-lg max-w-prose font-light">{post.summary}</p>
-    </article>
+    <p className="font-sans text-lg max-w-prose font-light">{post.summary}</p>
 
     <>{children}</>
-  </Link>
+  </article>
 );

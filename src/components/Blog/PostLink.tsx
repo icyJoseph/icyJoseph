ceb2bb966150897bd412b2807bddaf597e82c763
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-08-01 10:33:33 +0200

    style: heading wrapping anchor tag

--- src/components/Blog/PostLink.tsx
+++ src/components/Blog/PostLink.tsx
@@ -12,13 +12,15 @@ export const PostLink = ({
   children?: ReactNode | ReactNode[];
 }) => (
   <article className="mb-8 px-1 relative">
-    <Link
-      key={post.slug}
-      href={`/blog/${post.slug}`}
-      className="hover:underline decoration-pale-yellow after:absolute after:content-[''] after:top-0 after:left-0 after:bottom-0 after:right-0 after:inset-0"
-    >
-      <h3 className="text-pale-yellow text-xl">{post.title}</h3>
-    </Link>
+    <h3 className="text-pale-yellow text-xl">
+      <Link
+        key={post.slug}
+        href={`/blog/${post.slug}`}
+        className="hover:underline decoration-pale-yellow after:absolute after:content-[''] after:top-0 after:left-0 after:bottom-0 after:right-0 after:inset-0"
+      >
+        {post.title}
+      </Link>
+    </h3>
 
     <p className="font-sans text-lg max-w-prose font-light">{post.summary}</p>
 
